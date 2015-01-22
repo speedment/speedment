bfdf6856923e91640466f978495655e4c4ecae10
emilforslund <emil.forslund@pyknic.se>
2015-01-22 11:19:27 -0800

    Added new test for _Field

---
+++ src/test/java/com/speedment/codegen/view/java8/FieldViewTest.java
@@ -0,0 +1,71 @@
+package com.speedment.codegen.view.java8;
+
+import com.speedment.codegen.CodeGenerator;
+import com.speedment.codegen.model.Field_;
+import com.speedment.codegen.model.Type_;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Duncan
+ */
+public class FieldViewTest {
+	
+	private static CodeGenerator renderer;
+	private FieldView view;
+	
+	public FieldViewTest() {
+	}
+	
+	@BeforeClass
+	public static void setUpClass() {
+		renderer = new JavaCodeGen();
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+	}
+	
+	@Before
+	public void setUp() {
+		view = new FieldView();
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of render method, of class FieldView.
+	 */
+	@Test
+	public void testRender() {
+		Field_ field = new Field_(Type_.DOUBLE_PRIMITIVE, "width").final_().private_().static_();
+		CharSequence[] expResult = new CharSequence[] {
+			"private final static double width",
+			"private static final double width",
+			"final private static double width",
+			"final static private double width",
+			"static private final double width",
+			"static final private double width"
+		};
+
+		CharSequence result = view.render(renderer, field);
+		
+		boolean success = false;
+		for (CharSequence exp : expResult) {
+			if (exp.equals(result)) {
+				success = true;
+				break;
+			}
+		}
+		
+		assertTrue("FieldView.render gave an unexpected output of: '" + result + "'.", success);
+	}
+	
+}
