177be86126bee63ca42325d86e27f3ab58652c68
emilforslund <emil.forslund@pyknic.se>
2015-07-16 13:42:49 +0200

    Changed syntax from Json.build to JsonFormatter.apply

--- src/main/java/com/speedment/util/json/JsonFormatter.java
+++ src/main/java/com/speedment/util/json/JsonFormatter.java
@@ -43,94 +43,94 @@ import java.util.stream.Stream;
  * @author Emil Forslund
  * @param <ENTITY> Entity type
  */
-public final class Json<ENTITY> {
+public final class JsonFormatter<ENTITY> {
 
 	private final Map<String, Function<ENTITY, String>> getters;
 		
-	public Json() {
+	public JsonFormatter() {
 		this.getters = new LinkedHashMap<>();
 	}
 	
 	// Fields
-	public <T> Json<ENTITY> put(ReferenceField<ENTITY, T> field) {
+	public <T> JsonFormatter<ENTITY> put(ReferenceField<ENTITY, T> field) {
 		return put(jsonField(field), field::getFrom);
 	}
 	
-	public Json<ENTITY> put(IntField<ENTITY> field) {
+	public JsonFormatter<ENTITY> put(IntField<ENTITY> field) {
 		return put(jsonField(field), field::getFrom);
 	}
 	
-	public Json<ENTITY> put(LongField<ENTITY> field) {
+	public JsonFormatter<ENTITY> put(LongField<ENTITY> field) {
 		return put(jsonField(field), field::getFrom);
 	}
 	
-	public Json<ENTITY> put(DoubleField<ENTITY> field) {
+	public JsonFormatter<ENTITY> put(DoubleField<ENTITY> field) {
 		return put(jsonField(field), field::getFrom);
 	}
 	
 	// Foreign key fields.
-	public <T, FK_ENTITY> Json<ENTITY> put(ReferenceForeignKeyField<ENTITY, T, FK_ENTITY> field, Json<FK_ENTITY> builder) {
+	public <T, FK_ENTITY> JsonFormatter<ENTITY> put(ReferenceForeignKeyField<ENTITY, T, FK_ENTITY> field, JsonFormatter<FK_ENTITY> builder) {
 		return put(jsonField(field), field::findFrom, builder);
 	}
 	
-	public <T extends Comparable<? super T>, FK_ENTITY> Json<ENTITY> put(ComparableReferenceForeignKeyField<ENTITY, T, FK_ENTITY> field, Json<FK_ENTITY> builder) {
+	public <T extends Comparable<? super T>, FK_ENTITY> JsonFormatter<ENTITY> put(ComparableReferenceForeignKeyField<ENTITY, T, FK_ENTITY> field, JsonFormatter<FK_ENTITY> builder) {
 		return put(jsonField(field), field::findFrom, builder);
 	}
 	
-	public <FK_ENTITY> Json<ENTITY> put(StringReferenceForeignKeyField<ENTITY, FK_ENTITY> field, Json<FK_ENTITY> builder) {
+	public <FK_ENTITY> JsonFormatter<ENTITY> put(StringReferenceForeignKeyField<ENTITY, FK_ENTITY> field, JsonFormatter<FK_ENTITY> builder) {
 		return put(jsonField(field), field::findFrom, builder);
 	}
 	
 	// Label-and-getter pairs
-	public <T> Json<ENTITY> put(String label, Function<ENTITY, T> getter) {
+	public <T> JsonFormatter<ENTITY> put(String label, Function<ENTITY, T> getter) {
 		getters.put(label, e -> "\"" + label + "\":" + jsonValue(getter.apply(e)));
 		return this;
 	}
 	
-	public <T> Json<ENTITY> putDouble(String label, ToDoubleFunction<ENTITY> getter) {
+	public <T> JsonFormatter<ENTITY> putDouble(String label, ToDoubleFunction<ENTITY> getter) {
 		getters.put(label, e -> "\"" + label + "\":" + getter.applyAsDouble(e));
 		return this;
 	}
 	
-	public <T> Json<ENTITY> putInt(String label, ToIntFunction<ENTITY> getter) {
+	public <T> JsonFormatter<ENTITY> putInt(String label, ToIntFunction<ENTITY> getter) {
 		getters.put(label, e -> "\"" + label + "\":" + getter.applyAsInt(e));
 		return this;
 	}
 	
-	public <T> Json<ENTITY> putLong(String label, ToLongFunction<ENTITY> getter) {
+	public <T> JsonFormatter<ENTITY> putLong(String label, ToLongFunction<ENTITY> getter) {
 		getters.put(label, e -> "\"" + label + "\":" + getter.applyAsLong(e));
 		return this;
 	}
 	
 	// Label-and-getter with custom formatter
-	public <FK_ENTITY> Json<ENTITY> put(String label, Function<ENTITY, FK_ENTITY> getter, Json<FK_ENTITY> builder) {
-		getters.put(label, e -> "\"" + label + "\":" + builder.build(getter.apply(e)));
+	public <FK_ENTITY> JsonFormatter<ENTITY> put(String label, Function<ENTITY, FK_ENTITY> getter, JsonFormatter<FK_ENTITY> builder) {
+		getters.put(label, e -> "\"" + label + "\":" + builder.apply(getter.apply(e)));
 		return this;
 	}
 
 	// Label-and-streamer with custom formatter.
-	public <FK_ENTITY> Json<ENTITY> putAll(String label, Function<ENTITY, Stream<FK_ENTITY>> streamer, Json<FK_ENTITY> builder) {
-		getters.put(label, e -> "\"" + label + "\":[" + streamer.apply(e).map(builder::build).collect(joining(",")) + "]");
+	public <FK_ENTITY> JsonFormatter<ENTITY> putAll(String label, Function<ENTITY, Stream<FK_ENTITY>> streamer, JsonFormatter<FK_ENTITY> builder) {
+		getters.put(label, e -> "\"" + label + "\":[" + streamer.apply(e).map(builder::apply).collect(joining(",")) + "]");
 		return this;
 	}
 	
-	public <FK_ENTITY> Json<ENTITY> putAll(String label, Function<ENTITY, Stream<FK_ENTITY>> streamer, Function<FK_ENTITY, String> formatter) {
+	public <FK_ENTITY> JsonFormatter<ENTITY> putAll(String label, Function<ENTITY, Stream<FK_ENTITY>> streamer, Function<FK_ENTITY, String> formatter) {
 		getters.put(label, e -> "\"" + label + "\":[" + streamer.apply(e).map(formatter).collect(joining(",")) + "]");
 		return this;
 	}
 	
 	// Removers by label
-	public Json<ENTITY> remove(String label) {
+	public JsonFormatter<ENTITY> remove(String label) {
 		getters.remove(label);
 		return this;
 	}
 	
-	public <T> Json<ENTITY> remove(ReferenceField<ENTITY, T> field) {
+	public <T> JsonFormatter<ENTITY> remove(ReferenceField<ENTITY, T> field) {
 		getters.remove(jsonField(field));
 		return this;
 	}
 	
-	public String build(ENTITY entity) {
+	public String apply(ENTITY entity) {
 		return "{" + 
 			getters.values().stream()
 				.map(g -> g.apply(entity))
@@ -147,7 +147,7 @@ public final class Json<ENTITY> {
         
 		if (in instanceof Optional<?>) {
             final Optional<?> o = (Optional<?>) in;
-            return o.map(Json::jsonValue).orElse("null");
+            return o.map(JsonFormatter::jsonValue).orElse("null");
 		} else {
             if (in == null) {
                 value = "null";
@@ -169,9 +169,9 @@ public final class Json<ENTITY> {
         return value;
 	}
 	
-	public static <PK, ENTITY, BUILDER extends Buildable<ENTITY>, MANAGER extends Manager<PK, ENTITY, BUILDER>> Json<ENTITY> allFrom(MANAGER manager) {
+	public static <PK, ENTITY, BUILDER extends Buildable<ENTITY>, MANAGER extends Manager<PK, ENTITY, BUILDER>> JsonFormatter<ENTITY> allFrom(MANAGER manager) {
 		
-		final Json<ENTITY> json = new Json<>();
+		final JsonFormatter<ENTITY> json = new JsonFormatter<>();
 		
 		manager.getTable()
 			.streamOf(Column.class)
