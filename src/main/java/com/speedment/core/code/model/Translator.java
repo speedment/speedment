92aef5497e94b4fb2c5d7616bef35aba4cff25c9
Pyknic <emil.forslund@pyknic.se>
2015-07-20 10:43:44 +0200

    Add javadoc to Translator.java

--- src/main/java/com/speedment/core/code/model/Translator.java
+++ src/main/java/com/speedment/core/code/model/Translator.java
@@ -30,55 +30,130 @@ import com.speedment.core.config.model.Project;
 import com.speedment.core.config.model.Schema;
 import com.speedment.core.config.model.Table;
 import com.speedment.core.config.model.aspects.Node;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 /**
- *
+ * A component that can translate a {@link Node} into something else. This 
+ * interface is implemented to generate more files from the same database 
+ * structure.
+ * 
  * @author pemi
  * @param <T> The ConfigEntity type to use
+ * @param <R> The type to translate into.
+ * 
+ * @See Node
  */
 public interface Translator<T extends Node, R> extends Supplier<R> {
 
+    /**
+     * The node being translated.
+     * 
+     * @return the node.
+     */
     T getNode();
 
+    /**
+     * Return this node or any ancestral node that is a {@link Project}. If no 
+     * such node exists, an <code>IllegalStateException</code> is thrown.
+     * 
+     * @return the project node.
+     */
     default Project project() {
         return getGenericConfigEntity(Project.class);
     }
 
+    /**
+     * Return this node or any ancestral node that is a {@link Dbms}. If no 
+     * such node exists, an <code>IllegalStateException</code> is thrown.
+     * 
+     * @return the dbms node.
+     */
     default Dbms dbms() {
         return getGenericConfigEntity(Dbms.class);
     }
 
+    /**
+     * Return this node or any ancestral node that is a {@link Schema}. If no 
+     * such node exists, an <code>IllegalStateException</code> is thrown.
+     * 
+     * @return the schema node.
+     */
     default Schema schema() {
         return getGenericConfigEntity(Schema.class);
     }
 
+    /**
+     * Return this node or any ancestral node that is a {@link Table}. If no 
+     * such node exists, an <code>IllegalStateException</code> is thrown.
+     * 
+     * @return the table node.
+     */
     default Table table() {
         return getGenericConfigEntity(Table.class);
     }
 
+    /**
+     * Return this node or any ancestral node that is a {@link Column}. If no 
+     * such node exists, an <code>IllegalStateException</code> is thrown.
+     * 
+     * @return the column node.
+     */
     default Column column() {
         return getGenericConfigEntity(Column.class);
     }
 
+    /**
+     * Returns a stream over all enabled columns in the node tree. Disabled
+     * nodes will be ignored.
+     * 
+     * @return the enabled columns.
+     * @see Column
+     */
     default Stream<Column> columns() {
         return table().streamOf(Column.class).filter(Column::isEnabled);
     }
 
+    /**
+     * Returns a stream over all enabled indexes in the node tree. Disabled
+     * nodes will be ignored.
+     * 
+     * @return the enabled indexes.
+     * @see Index
+     */
     default Stream<Index> indexes() {
         return table().streamOf(Index.class).filter(Index::isEnabled);
     }
 
+    /**
+     * Returns a stream over all enabled foreign keys in the node tree. Disabled
+     * nodes will be ignored.
+     * 
+     * @return the enabled foreign keys.
+     * @see ForeignKey
+     */
     default Stream<ForeignKey> foreignKeys() {
         return table().streamOf(ForeignKey.class).filter(ForeignKey::isEnabled);
     }
 
+    /**
+     * Returns a stream over all enabled primary key columns in the node tree. 
+     * Disabled nodes will be ignored.
+     * 
+     * @return the enabled primary key columns.
+     * @see PrimaryKeyColumn
+     */
     default Stream<PrimaryKeyColumn> primaryKeyColumns() {
         return table().streamOf(PrimaryKeyColumn.class).filter(PrimaryKeyColumn::isEnabled);
     }
 
+    /**
+     * Returns this node or one of the ancestor nodes if it matches the 
+     * specified <code>Class</code>. If no such node exists, an 
+     * <code>IllegalStateException</code> is thrown.
+     * 
+     * @return the node found.
+     */
     default <E extends Node> E getGenericConfigEntity(Class<E> clazz) {
         if (clazz.isAssignableFrom(getNode().getInterfaceMainClass())) {
             @SuppressWarnings("unchecked")
@@ -88,18 +163,8 @@ public interface Translator<T extends Node, R> extends Supplier<R> {
 
         return getNode().ancestor(clazz)
             .orElseThrow(() -> new IllegalStateException(
-                    getNode() + " is not a " + clazz.getSimpleName()
-                    + " and does not have a parent that is a " + clazz.getSimpleName()
-                ));
+                getNode() + " is not a " + clazz.getSimpleName()
+                + " and does not have a parent that is a " + clazz.getSimpleName()
+            ));
     }
-
-    default <T> T with(T initial, Consumer<T> consumer) {
-        consumer.accept(initial);
-        return initial;
-    }
-
-    default <T> T with(Supplier<T> supplier, Consumer<T> consumer) {
-        return with(supplier.get(), consumer);
-    }
-
 }
