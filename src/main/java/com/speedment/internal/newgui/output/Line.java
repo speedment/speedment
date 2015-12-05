9382874512ee7061c35aff7c7a3cfd7cd07769af
Emil Forslund <emil.forslund@pyknic.se>
2015-12-04 16:57:32 -0800

    Clean up

--- src/main/java/com/speedment/internal/newgui/output/Line.java
+++ src/main/java/com/speedment/internal/newgui/output/Line.java
@@ -24,74 +24,28 @@ import javafx.scene.control.Label;
  */
 public final class Line extends Label {
     
-//    private final static Font FONT = Font.font("Monospaced", 12);
-
     private Line(String text, String... classes) {
         super(text);
         getStyleClass().addAll(classes);
-//
-//        setFont(FONT);
-//        setStyle(
-//            "-fx-background-color: " + rgb(background) + ";" +
-//            "-fx-text-fill: " + rgb(foreground) + ";"
-//        );
     }
-    
-//    private static String rgb(Color color) {
-//        return new StringBuilder("rgba(")
-//            .append(color.getRed() * 255).append(", ")
-//            .append(color.getGreen() * 255).append(", ")
-//            .append(color.getBlue() * 255).append(", ")
-//            .append(color.getOpacity()).append(")")
-//            .toString();
-//    }
-    
+
     public static Line info(String message) {
         final Line line = new Line(message, "msg", "info");
-//        line.getStyleClass().addAll("msg", "info");
         return line;
     }
     
     public static Line warning(String message) {
         final Line line = new Line(message, "msg", "warning");
-//        line.getStyleClass().addAll("msg", "warning");
         return line;
     }
     
     public static Line error(String message) {
         final Line line = new Line(message, "msg", "error");
-//        line.getStyleClass().addAll("msg", "error");
         return line;
     }
     
     public static Line success(String message) {
         final Line line = new Line(message, "msg", "success");
-//        line.getStyleClass().addAll("msg", "success");
         return line;
     }
-    
-//    private final static class Info extends Line {
-//        private Info(String text) {
-//            super(text, Color.BLACK, Color.TRANSPARENT);
-//        }
-//    }
-//    
-//    private final static class Warning extends Line {
-//        private Warning(String text) {
-//            super(text, Color.BLACK, Color.LIGHTGOLDENRODYELLOW);
-//            getStyleClass().add("warning");
-//        }
-//    }
-//    
-//    private final static class Error extends Line {
-//        private Error(String text) {
-//            super(text, Color.BLACK, Color.LIGHTPINK);
-//        }
-//    }
-//    
-//    private final static class Success extends Line {
-//        private Success(String text) {
-//            super(text, Color.BLACK, Color.LIGHTGREEN);
-//        }
-//    }
 }
