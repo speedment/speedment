4d92893ccc24f12d8741720161374f471b959e5e
Per-Åke Minborg <minborg@speedment.com>
2015-12-11 15:06:32 -0800

    Remove FK redundant type

--- src/main/java/com/speedment/component/StreamSupplierComponent.java
+++ src/main/java/com/speedment/component/StreamSupplierComponent.java
@@ -47,8 +47,7 @@ public interface StreamSupplierComponent extends Component {
      */
     <ENTITY> Stream<ENTITY> stream(Class<ENTITY> entityClass, StreamDecorator decorator);
 
-    default <ENTITY, V extends Comparable<? super V>, FK>
-            Optional<ENTITY> findAny(Class<ENTITY> entityClass, ComparableField<ENTITY, V> field, V value) {
+    default <ENTITY, V extends Comparable<? super V>> Optional<ENTITY> findAny(Class<ENTITY> entityClass, ComparableField<ENTITY, V> field, V value) {
         return stream(entityClass, StreamDecorator.IDENTITY)
                 .filter(field.equal(value))
                 .findAny();
