173e9c6fa8453207cf4ad2c0e586bce2aba8b2a8
Emil Forslund <emil.forslund@pyknic.se>
2016-01-12 17:18:47 -0800

    Remove fix

--- src/main/java/com/speedment/internal/core/config/BaseDocument.java
+++ src/main/java/com/speedment/internal/core/config/BaseDocument.java
@@ -95,12 +95,12 @@ public class BaseDocument implements Document {
     @Override
     public void put(String key, Object value) {
         requireNonNull(value);
-        
-        if (value instanceof Number) {
-            config.put(key, ((Number) value).doubleValue());
-        } else {
+//        
+//        if (value instanceof Number) {
+//            config.put(key, ((Number) value).doubleValue());
+//        } else {
             config.put(key, value);
-        }
+//        }
     }
     
     @Override
