1e8686760bb2d037be3526a7356a2d50030f03a4
Per-Åke Minborg <minborg@speedment.com>
2016-03-13 15:20:29 -0700

    Expose FunctionCastUtil

--- src/main/java/com/speedment/util/FunctionCastUtil.java
+++ src/main/java/com/speedment/util/FunctionCastUtil.java
@@ -14,7 +14,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.speedment.internal.util.function;
+package com.speedment.util;
 
 import java.util.function.*;
 
