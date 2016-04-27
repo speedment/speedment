c3ad37f94bfa778b4f93a0a67c19cc69f99ec97e
Emil Forslund <emil.forslund@pyknic.se>
2016-04-27 11:04:52 -0700

    Trait is no longer part of the API

--- src/main/java/com/speedment/internal/ui/config/trait/package-info.java
+++ src/main/java/com/speedment/internal/ui/config/trait/package-info.java
@@ -2,8 +2,5 @@
  * Common traits used in the implementations of the typed 
  * {@link com.speedment.ui.config.DocumentProperty} instances are located in
  * this package.
- * <p>
- * This package is part of the API. Modifications to classes here should only
- * (if ever) be done in major releases.
  */
 package com.speedment.internal.ui.config.trait;
