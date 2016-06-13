b8d053ce1742561d8e27627dd6e598112d204253
Emil Forslund <emil.forslund@pyknic.se>
2016-06-13 13:24:40 -0700

    Tool: Speedment instance should only be created once

--- tool/src/main/java/com/speedment/tool/MainApp.java
+++ tool/src/main/java/com/speedment/tool/MainApp.java
@@ -20,14 +20,11 @@ import com.speedment.common.injector.Injector;
 import com.speedment.common.logger.Logger;
 import com.speedment.common.logger.LoggerManager;
 import com.speedment.generator.internal.component.CodeGenerationComponentImpl;
+import com.speedment.runtime.component.ProjectComponent;
 import com.speedment.runtime.internal.runtime.DefaultApplicationBuilder;
 import com.speedment.runtime.internal.util.EmailUtil;
-import static com.speedment.tool.component.UserInterfaceComponent.ReuseStage.USE_EXISTING_STAGE;
 import com.speedment.tool.internal.component.UserInterfaceComponentImpl;
-import com.speedment.tool.internal.util.ConfigFileHelper;
 import com.speedment.tool.internal.util.InjectionLoader;
-import java.io.File;
-import java.util.List;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import static java.util.Objects.requireNonNull;
@@ -59,23 +56,19 @@ public final class MainApp extends Application {
         }
         
         final UserInterfaceComponentImpl ui = INJECTOR.get(UserInterfaceComponentImpl.class);
-        final ConfigFileHelper config       = INJECTOR.get(ConfigFileHelper.class);
+        final ProjectComponent projects     = INJECTOR.get(ProjectComponent.class);
         final InjectionLoader loader        = INJECTOR.get(InjectionLoader.class);
         
         ui.start(this, stage);
         
-        final Parameters parameters = getParameters();
-        final List<String> params   = parameters.getRaw();
-        if (params.isEmpty()) {
+        if (projects.getProject().dbmses().noneMatch(dbms -> true)) {
             if (EmailUtil.hasEmail()) {
                 loader.loadAndShow("Connect");
             } else {
                 loader.loadAndShow("MailPrompt");
             }
         } else {
-            final String filename   = params.get(0).trim().replace("\\", "/");
-            final File file         = new File(filename);
-            config.loadConfigFile(file, USE_EXISTING_STAGE);
+            loader.loadAndShow("Scene");
         }
     }
     
