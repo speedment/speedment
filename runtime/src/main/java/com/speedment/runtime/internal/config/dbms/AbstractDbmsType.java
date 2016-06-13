f3b60472f52ddd43b3d6e0c6258f21b097b0379f
Emil Forslund <emil.forslund@pyknic.se>
2016-06-13 10:57:32 -0700

    Runtime: DbmsTypes should be supported by default

--- runtime/src/main/java/com/speedment/runtime/internal/config/dbms/AbstractDbmsType.java
+++ runtime/src/main/java/com/speedment/runtime/internal/config/dbms/AbstractDbmsType.java
@@ -51,7 +51,7 @@ public abstract class AbstractDbmsType implements DbmsType {
 
     @Override
     public boolean isSupported() {
-        return false;
+        return true;
     }
 
     @Override
