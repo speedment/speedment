bc4e58c90488d35bea2f76f0bc32afe2604f3c91
Emil Forslund <emil.forslund@pyknic.se>
2016-09-27 15:31:12 -0700

    Make whole Speedment buildable

--- runtime/src/main/java/com/speedment/runtime/internal/field/streamer/BackwardFinderImpl.java
+++ runtime/src/main/java/com/speedment/runtime/internal/field/streamer/BackwardFinderImpl.java
@@ -1,16 +1,18 @@
-/*
- * Copyright (c) Emil Forslund, 2016.
- * All Rights Reserved.
- * 
- * NOTICE:  All information contained herein is, and remains
- * the property of Emil Forslund and his suppliers, if any. 
- * The intellectual and technical concepts contained herein 
- * are proprietary to Emil Forslund and his suppliers and may 
- * be covered by U.S. and Foreign Patents, patents in process, 
- * and are protected by trade secret or copyright law. 
- * Dissemination of this information or reproduction of this 
- * material is strictly forbidden unless prior written 
- * permission is obtained from Emil Forslund himself.
+/**
+ *
+ * Copyright (c) 2006-2016, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.speedment.runtime.internal.field.streamer;
 
