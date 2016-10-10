8e539cb6b9bfdd286b5075b72aa66a3bc040920b
Emil Forslund <emil@speedment.com>
2016-10-10 13:27:17 -0700

    Runtime: Begin working on next SNAPSHOT release

--- runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/component/InfoComponentImpl.java
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/component/InfoComponentImpl.java
@@ -38,7 +38,7 @@ public final class InfoComponentImpl implements InfoComponent {
 
     @Override
     public String getImplementationVersion() {
-        return "3.0.0-EA2";
+        return "3.0.3-SNAPSHOT";
     }
 
     @Override
