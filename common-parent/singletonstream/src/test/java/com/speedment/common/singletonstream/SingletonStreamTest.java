31b0ea9b38602fa717e1d9e9251f77ee46353ae6
Per-Åke Minborg <minborg@speedment.com>
2016-11-18 15:37:30 -0800

    singletonstream: Add test

--- common-parent/singletonstream/src/test/java/com/speedment/common/singletonstream/SingletonStreamTest.java
+++ common-parent/singletonstream/src/test/java/com/speedment/common/singletonstream/SingletonStreamTest.java
@@ -19,7 +19,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.speedment.runtime.core.internal.util.stream;
+package com.speedment.common.singletonstream;
 
 import org.junit.*;
 
