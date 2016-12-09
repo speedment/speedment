0d91e1a7034c4b7ccea9546b4ec32c3977871d9b
pyknic <emil@speedment.com>
2016-12-09 11:46:23 -0800

    function: Add LongBiConsumer-interface

---
+++ common-parent/function/src/main/java/com/speedment/common/function/LongBiConsumer.java
@@ -0,0 +1,46 @@
+package com.speedment.common.function;
+
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+/**
+ * Represents an operation that accepts two {@code long}-valued arguments and
+ * returns no result.  This is the primitive type specialization of
+ * {@link BiConsumer} for {@code long}.  Unlike most other functional interfaces,
+ * {@code LongBiConsumer} is expected to operate via side-effects.
+ *
+ * <p>This is a functional interface whose functional method is 
+ * {@link #accept(long, long)}.
+ * 
+ * @author Per Minborg
+ * @since  1.0.1
+ */
+@FunctionalInterface
+public interface LongBiConsumer {
+
+    /**
+     * Performs this operation on the given argument.
+     * 
+     * @param first   the first input argument
+     * @param second  the second input argument
+     */
+    void accept(long first, long second);
+
+    /**
+     * Returns a composed {@code LongBiConsumer} that performs, in sequence, 
+     * this operation followed by the {@code after} operation. If performing 
+     * either operation throws an exception, it is relayed to the caller of the
+     * composed operation.  If performing this operation throws an exception,
+     * the {@code after} operation will not be performed.
+     *
+     * @param after  the operation to perform after this operation
+     * @return       a composed {@code LongBiConsumer} that performs in sequence 
+     *               this operation followed by the {@code after} operation
+     * 
+     * @throws NullPointerException  if {@code after} is null
+     */
+    default LongBiConsumer andThen(LongBiConsumer after) {
+        Objects.requireNonNull(after);
+        return (long a, long b) -> { accept(a, b); after.accept(a, b); };
+    }
+}
