cfd16ccf4186b1c034e2c558c9dfa8f23b4c53f9
Per-Åke Minborg <minborg@speedment.com>
2017-01-04 09:10:12 -0800

    Update copyright year to 2017

--- runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/db/DbmsColumnHandler.java
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/db/DbmsColumnHandler.java
@@ -1,3 +1,19 @@
+/**
+ *
+ * Copyright (c) 2006-2017, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.speedment.runtime.core.db;
 
 import com.speedment.runtime.config.Column;
@@ -18,7 +34,7 @@ public interface DbmsColumnHandler {
      * allows exclusion from that rule in order to for example exclude auto incremented
      * fields.
      *
-     * @return a predicate used to determine if a column shall be exluded in generated SQL INSERT statements
+     * @return a predicate used to determine if a column shall be excluded in generated SQL INSERT statements
      */
     Predicate<Column> excludedInInsertStatement();
 }
