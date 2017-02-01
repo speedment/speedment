b2c52503eaa9db9cf4199816e30045f6908bb0b8
Per-Åke Minborg <minborg@speedment.com>
2017-02-01 09:01:11 -0800

    Bump version to 3.0.3-SNAPSHOT

--- runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/component/InfoComponentImpl.java
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/component/InfoComponentImpl.java
@@ -38,7 +38,7 @@ public final class InfoComponentImpl implements InfoComponent {
 
     @Override
     public String getImplementationVersion() {
-        return "3.0.2";
+        return "3.0.3-SNAPSHOT";
     }
 
     @Override
