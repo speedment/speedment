a6198e0dd9b2e121af7170b72ed339972cdccc05
Mark Schrijver <mark.schrijver@gxsoftware.com>
2017-03-26 01:17:02 +0100

    Added a line of code to actually propagate the ConfigFile  configuration property so it is used when reloading the database config. Added the appropriate unit test

---
+++ build-parent/maven-plugin/src/test/java/com/speedment/maven/abstractmojo/AbstractReloadMojoTestImpl.java
@@ -0,0 +1,20 @@
+package com.speedment.maven.abstractmojo;
+
+import com.speedment.runtime.core.Speedment;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * test implementaiton of AbstractReloadMojo
+ */
+public class AbstractReloadMojoTestImpl extends AbstractReloadMojo {
+
+    @Override
+    protected String launchMessage() {
+        return "test implementaiton of AbstractReloadMojo";
+    }
+
+    public void execute(Speedment speedment) throws MojoFailureException, MojoExecutionException {
+        super.execute(speedment);
+    }
+}
