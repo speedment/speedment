fb770acb8a32bd58ebf0dfbd085d6dd32e79208b
Per-Åke Minborg <minborg@speedment.com>
2017-04-17 17:04:24 -0700

    Fix license headers

--- runtime-parent/runtime-core/src/test/java/com/speedment/runtime/core/internal/component/sql/optimiser/FilterOrderSkipOptimizer_OptimizeTest.java
+++ runtime-parent/runtime-core/src/test/java/com/speedment/runtime/core/internal/component/sql/optimiser/FilterOrderSkipOptimizer_OptimizeTest.java
@@ -1,7 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Copyright (c) 2006-2017, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.speedment.runtime.core.internal.component.sql.optimiser;
 
@@ -15,7 +26,6 @@ import com.speedment.runtime.core.internal.stream.builder.action.reference.PeekA
 import com.speedment.runtime.core.internal.stream.builder.action.reference.SkipAction;
 import com.speedment.runtime.core.internal.stream.builder.action.reference.SortedComparatorAction;
 import com.speedment.runtime.core.internal.stream.builder.pipeline.PipelineImpl;
-import com.speedment.runtime.core.internal.util.testing.Permutations;
 import com.speedment.runtime.core.stream.Pipeline;
 import com.speedment.runtime.core.stream.action.Action;
 import com.speedment.runtime.core.stream.parallel.ParallelStrategy;
@@ -23,12 +33,9 @@ import com.speedment.runtime.test_support.MockDbmsType;
 import com.speedment.runtime.test_support.MockEntity;
 import com.speedment.runtime.test_support.MockEntityUtil;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.BaseStream;
-import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
-import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
