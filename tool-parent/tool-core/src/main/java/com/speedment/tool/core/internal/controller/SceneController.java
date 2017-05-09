80807e132d59d668fcb30a54bfc579c2257f96e8
pyknic <emil@speedment.com>
2017-05-09 15:42:23 -0700

    tool-core: Format imports

--- tool-parent/tool-core/src/main/java/com/speedment/tool/core/internal/controller/SceneController.java
+++ tool-parent/tool-core/src/main/java/com/speedment/tool/core/internal/controller/SceneController.java
@@ -24,17 +24,19 @@ import com.speedment.runtime.core.internal.util.Statistics;
 import com.speedment.tool.core.component.UserInterfaceComponent;
 import com.speedment.tool.core.component.VersionComponent;
 import com.speedment.tool.core.internal.util.InjectionLoader;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.VBox;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+
 import static javafx.application.Platform.runLater;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.SplitPane;
-import javafx.scene.layout.VBox;
 
 /**
  *
