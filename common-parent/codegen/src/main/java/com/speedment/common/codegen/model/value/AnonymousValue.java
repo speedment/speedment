a943e343b964deedce0456453a92d5973e3764f2
pyknic <emil@speedment.com>
2017-05-09 13:42:43 -0700

    codegen: Create models for representing anonymous implementations (#434)

--- common-parent/codegen/src/main/java/com/speedment/common/codegen/model/value/AnonymousValue.java
+++ common-parent/codegen/src/main/java/com/speedment/common/codegen/model/value/AnonymousValue.java
@@ -1,9 +1,14 @@
 package com.speedment.common.codegen.model.value;
 
-import com.speedment.common.codegen.model.ClassOrInterface;
 import com.speedment.common.codegen.model.Value;
-import com.speedment.common.codegen.model.trait.HasGenerics;
+import com.speedment.common.codegen.model.trait.HasClasses;
+import com.speedment.common.codegen.model.trait.HasFields;
+import com.speedment.common.codegen.model.trait.HasInitializers;
+import com.speedment.common.codegen.model.trait.HasMethods;
 import com.speedment.common.codegen.model.trait.HasValues;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 /**
  * A value representing a reference to an abstract implementation used
@@ -12,11 +17,36 @@ import com.speedment.common.codegen.model.trait.HasValues;
  * @author Emil Forslund
  * @since  2.4.6
  */
-public interface AnonymousValue<T extends ClassOrInterface<T>>
-extends Value<T>,
-        HasGenerics<AnonymousValue<T>>,
-        HasValues<AnonymousValue<T>> {
+public interface AnonymousValue
+extends Value<Type>,
+        HasValues<AnonymousValue>,
+        HasMethods<AnonymousValue>,
+        HasFields<AnonymousValue>,
+        HasInitializers<AnonymousValue>,
+        HasClasses<AnonymousValue> {
 
     @Override
-    AnonymousValue<T> setValue(T value);
+    AnonymousValue setValue(Type value);
+
+    /**
+     * Returns a list of type parameters to set when instantiating the anonymous
+     * type.
+     * <p>
+     * The returned list is mutable.
+     *
+     * @return list of type parameters
+     */
+    List<Type> getTypeParameters();
+
+    /**
+     * Adds the specified type parameter to the end of the
+     * {@link #getTypeParameters} list.
+     *
+     * @param typeParameter  the type parameter to add
+     * @return               a reference to this
+     */
+    default AnonymousValue add(Type typeParameter) {
+        getTypeParameters().add(typeParameter);
+        return this;
+    }
 }
