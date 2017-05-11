e577d4bf9c032a90c796d33a9756b5556ea6f2ca
pyknic <emil@speedment.com>
2017-05-11 16:02:54 -0700

    runtime-typemapper: Fix #440 Create type mapper for mapping from Binary(16) to UUID

---
+++ runtime-parent/runtime-typemapper/src/main/java/com/speedment/runtime/typemapper/other/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Implementations of the {@link com.speedment.runtime.typemapper.TypeMapper} 
+ * interface that map from types that doesn't belong in any other package are
+ * located in this package.
+ * <p>
+ * This package is part of the API. Modifications to classes here should only
+ * (if ever) be done in major releases.
+ */
+package com.speedment.runtime.typemapper.other;
