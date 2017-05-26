0a5d86a7cbb84384216b2545a69cb4f832b57758
pyknic <emil@speedment.com>
2017-05-25 17:32:47 -0700

    runtime-field: #466 Add static constructor to interface

--- runtime-parent/runtime-field/src/main/java/com/speedment/runtime/field/EnumField.java
+++ runtime-parent/runtime-field/src/main/java/com/speedment/runtime/field/EnumField.java
@@ -1,6 +1,11 @@
 package com.speedment.runtime.field;
 
+import com.speedment.runtime.config.identifier.ColumnIdentifier;
+import com.speedment.runtime.field.internal.EnumFieldImpl;
+import com.speedment.runtime.field.method.ReferenceGetter;
+import com.speedment.runtime.field.method.ReferenceSetter;
 import com.speedment.runtime.field.trait.HasStringOperators;
+import com.speedment.runtime.typemapper.TypeMapper;
 
 import java.util.function.Function;
 
@@ -35,4 +40,34 @@ extends ComparableField<ENTITY, D, E>,
      */
     Function<E, String> enumToString();
 
+    /**
+     * Create a new instance of this interface using the default implementation.
+     *
+     * @param <ENTITY>      the entity type
+     * @param <D>           the database type
+     * @param <E>           the java enum type
+     * @param identifier    the column that this field represents
+     * @param getter        method reference to the getter in the entity
+     * @param setter        method reference to the setter in the entity
+     * @param typeMapper    the type mapper that is applied
+     * @param enumToString  method to convert enum to a string
+     * @param stringToEnum  method to convert a string to enum
+     * @param enumClass     the enum class
+     *
+     * @return            the created field
+     */
+    static <ENTITY, D, E extends Enum<E>> EnumField<ENTITY, D, E> create(
+            ColumnIdentifier<ENTITY> identifier,
+            ReferenceGetter<ENTITY, E> getter,
+            ReferenceSetter<ENTITY, E> setter,
+            TypeMapper<D, E> typeMapper,
+            Function<E, String> enumToString,
+            Function<String, E> stringToEnum,
+            Class<E> enumClass) {
+
+        return new EnumFieldImpl<>(
+            identifier, getter, setter, typeMapper,
+            enumToString, stringToEnum, enumClass
+        );
+    }
 }
