4f0fc45653f61e08eeba1203607d4e5acb0916c6
Per-Åke Minborg <minborg@speedment.com>
2017-06-09 13:52:58 -0700

    Add licsense headers

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,20 @@
+@REM
+@REM
+@REM Copyright (c) 2006-2017, Speedment, Inc. All Rights Reserved.
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License"); You may not
+@REM use this file except in compliance with the License. You may obtain a copy of
+@REM the License at:
+@REM
+@REM http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+@REM WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+@REM License for the specific language governing permissions and limitations under
+@REM the License.
+@REM
+
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
