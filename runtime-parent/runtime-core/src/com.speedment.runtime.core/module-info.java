bfc74e168fb1bfe3e3326fc9adf8e6445bde8216
Dan Lawesson <dan@speedment.com>
2017-06-13 19:02:14 +0200

    tool-propertyeditor: java9: require runtime.core.util and tool.config

--- runtime-parent/runtime-core/src/com.speedment.runtime.core/module-info.java
+++ runtime-parent/runtime-core/src/com.speedment.runtime.core/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.runtime.core {
     requires com.speedment.runtime.field;
     requires com.speedment.common.invariant;
+    exports com.speedment.runtime.core.util;
 }
---
+++ tool-parent/tool-propertyeditor/src/com.speedment.tool.propertyeditor/module-info.java
@@ -0,0 +1,4 @@
+module com.speedment.tool.propertyeditor {
+    requires com.speedment.runtime.core.util;
+    requires com.speedment.tool.config;
+}
