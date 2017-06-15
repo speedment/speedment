c1cd1d838d189cd21fb20182b00f634fcf095b79
Dan Lawesson <dan@kodgruvan.se>
2017-06-15 18:59:26 +0200

    core: Require com.speedment.runtime.config to get access to com.speedment.runtime.config

--- runtime-parent/runtime-core/src/com.speedment.runtime.core/main/java/module-info.java
+++ runtime-parent/runtime-core/src/com.speedment.runtime.core/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.runtime.core {
+    requires com.speedment.runtime.config;
     requires java.sql;
     requires com.speedment.common.invariant;
     requires com.speedment.runtime.field;
