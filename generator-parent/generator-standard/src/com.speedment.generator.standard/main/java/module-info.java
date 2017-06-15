2e9e4925e9c1b3c0f6b4025474292ef59b2e52fd
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:10:12 +0200

    generator.standard: Require com.speedment.common.injector to get access to com.speedment.common.injector

--- generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
+++ generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.generator.standard {
+    requires com.speedment.common.injector;
     requires com.speedment.generator.translator;
     requires com.speedment.common.codegen;
 }
