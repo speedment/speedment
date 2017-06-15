9dc656f68ab6955186add5eaad707a2e32f86859
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:21:17 +0200

    tool.propertyeditor: Require com.speedment.common.injector to get access to com.speedment.common.injector

--- tool-parent/tool-propertyeditor/src/com.speedment.tool.propertyeditor/main/java/module-info.java
+++ tool-parent/tool-propertyeditor/src/com.speedment.tool.propertyeditor/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.tool.propertyeditor {
+    requires com.speedment.common.injector;
     requires com.speedment.tool.config;
     requires javafx.graphics;
     requires javafx.controls;
