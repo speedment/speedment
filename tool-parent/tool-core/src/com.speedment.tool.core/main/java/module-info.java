cac86186205f342200867d19cc3c99f5baf7be15
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:26:26 +0200

    tool.core: Require javafx.base to get access to javafx.beans.binding

--- tool-parent/tool-core/src/com.speedment.tool.core/main/java/module-info.java
+++ tool-parent/tool-core/src/com.speedment.tool.core/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.tool.core {
+    requires javafx.base;
     requires com.speedment.tool.propertyeditor;
     requires com.speedment.tool.config;
     requires com.speedment.runtime.core;
