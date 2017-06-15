1779e47e04d8e5a6785631b3669c280092d4dced
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:05:44 +0200

    generator.translator: Require com.speedment.common.injector to get access to com.speedment.common.injector

--- generator-parent/generator-translator/src/com.speedment.generator.translator/main/java/module-info.java
+++ generator-parent/generator-translator/src/com.speedment.generator.translator/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.generator.translator {
+    requires com.speedment.common.injector;
     requires com.speedment.runtime.core;
     requires com.speedment.runtime.config;
     requires com.speedment.common.codegen;
