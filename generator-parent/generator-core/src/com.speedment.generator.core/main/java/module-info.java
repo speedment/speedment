14cfc16a0e177f8db3a59e5d9f29007c1fb717a7
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:14:21 +0200

    generator.core: Require com.speedment.generator.translator to get access to com.speedment.generator.translator

--- generator-parent/generator-core/src/com.speedment.generator.core/main/java/module-info.java
+++ generator-parent/generator-core/src/com.speedment.generator.core/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.generator.core {
+    requires com.speedment.generator.translator;
     requires com.speedment.common.codegen;
     requires com.speedment.runtime.config;
     requires com.speedment.common.injector;
