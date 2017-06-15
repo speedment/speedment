f7c62730aa84d113d033c112e865ad5e418c283c
Dan Lawesson <dan@kodgruvan.se>
2017-06-15 19:03:52 +0200

    injector: Export com.speedment.common.injector.execution

--- common-parent/injector/src/com.speedment.common.injector/main/java/module-info.java
+++ common-parent/injector/src/com.speedment.common.injector/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.common.injector {
+    exports com.speedment.common.injector.execution;
     exports com.speedment.common.injector.exception;
     exports com.speedment.common.injector.annotation;
     exports com.speedment.common.injector;
