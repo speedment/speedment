eae9fe8e2c247c1ca8460d592aa460fb39db688c
Dan Lawesson <dan@kodgruvan.se>
2017-06-15 19:01:41 +0200

    core: Require com.speedment.common.tuple to get access to com.speedment.common.tuple

--- runtime-parent/runtime-core/src/com.speedment.runtime.core/main/java/module-info.java
+++ runtime-parent/runtime-core/src/com.speedment.runtime.core/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.runtime.core {
+    requires com.speedment.common.tuple;
     requires com.speedment.runtime.typemapper;
     requires com.speedment.common.logger;
     requires com.speedment.common.injector;
