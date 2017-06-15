9fc63571f9ab799ae63b7394ef4c8d5dc24353ad
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:14:22 +0200

    generator.core: Require com.speedment.runtime.core to get access to com.speedment.runtime.core.component

--- generator-parent/generator-core/src/com.speedment.generator.core/main/java/module-info.java
+++ generator-parent/generator-core/src/com.speedment.generator.core/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.generator.core {
+    requires com.speedment.runtime.core;
     requires com.speedment.common.logger;
     requires com.speedment.generator.translator;
     requires com.speedment.common.codegen;
