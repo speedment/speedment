01ac8f17678d27b7c416cf0c5a2b072055895aa3
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:11:10 +0200

    generator.standard: Require com.speedment.runtime.typemapper to get access to com.speedment.runtime.typemapper

--- generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
+++ generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.generator.standard {
+    requires com.speedment.runtime.typemapper;
     requires java.sql;
     requires com.speedment.common.json;
     requires com.speedment.runtime.core;
