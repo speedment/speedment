28c054a431a4cd27b7fb14663eca4450e1578ae9
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:18:12 +0200

    tool.config: Require com.speedment.common.mapstream to get access to com.speedment.common.mapstream

--- tool-parent/tool-config/src/com.speedment.tool.config/main/java/module-info.java
+++ tool-parent/tool-config/src/com.speedment.tool.config/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.tool.config {
+    requires com.speedment.common.mapstream;
     requires com.speedment.common.function;
     requires com.speedment.runtime.core;
     requires javafx.base;
