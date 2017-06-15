b942e5070d71ecd641e618ae445b0efe92d092e8
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:27:28 +0200

    tool.core: Require com.speedment.common.function to get access to com.speedment.common.function

--- tool-parent/tool-core/src/com.speedment.tool.core/main/java/module-info.java
+++ tool-parent/tool-core/src/com.speedment.tool.core/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.tool.core {
+    requires com.speedment.common.function;
     requires com.speedment.common.mapstream;
     requires java.prefs;
     requires com.speedment.generator.core;
