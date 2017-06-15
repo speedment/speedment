65dcff96d56b0006d7f5540bb35ff6dcb67b1b7f
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:21:20 +0200

    tool.propertyeditor: Require com.speedment.generator.translator to get access to com.speedment.generator.translator.component

--- tool-parent/tool-propertyeditor/src/com.speedment.tool.propertyeditor/main/java/module-info.java
+++ tool-parent/tool-propertyeditor/src/com.speedment.tool.propertyeditor/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.tool.propertyeditor {
+    requires com.speedment.generator.translator;
     requires com.speedment.runtime.core;
     requires com.speedment.runtime.config;
     requires com.speedment.common.mapstream;
