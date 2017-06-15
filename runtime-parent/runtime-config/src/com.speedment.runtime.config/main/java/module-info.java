87b7af7394585705693e12101b85345b4b4039f3
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:19:12 +0200

    runtime.config: Export com.speedment.runtime.config.mutator.trait

--- runtime-parent/runtime-config/src/com.speedment.runtime.config/main/java/module-info.java
+++ runtime-parent/runtime-config/src/com.speedment.runtime.config/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.runtime.config {
+    exports com.speedment.runtime.config.mutator.trait;
     exports com.speedment.runtime.config.parameter;
     exports com.speedment.runtime.config.mutator;
     exports com.speedment.runtime.config.internal;
