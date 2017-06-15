57a9ff83613bc8e336a1b26d39f995a1b4aec068
Dan Lawesson <dan@kodgruvan.se>
2017-06-15 19:01:40 +0200

    config: Export com.speedment.runtime.config.internal

--- runtime-parent/runtime-config/src/com.speedment.runtime.config/main/java/module-info.java
+++ runtime-parent/runtime-config/src/com.speedment.runtime.config/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.runtime.config {
+    exports com.speedment.runtime.config.internal;
     exports com.speedment.runtime.config.trait;
     exports com.speedment.runtime.config.identifier.trait;
     exports com.speedment.runtime.config.util;
