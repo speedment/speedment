180d544dea75b7f70f52bb97a3952ffe6f2fb3c2
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:10:13 +0200

    generator.standard: Require com.speedment.common.json to get access to com.speedment.common.json

--- generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
+++ generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.generator.standard {
+    requires com.speedment.common.json;
     requires com.speedment.runtime.core;
     requires com.speedment.runtime.config;
     requires com.speedment.common.mapstream;
