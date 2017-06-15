809f74c8d7f25498068b83594aafb5d9a19fb930
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 01:10:15 +0200

    generator.standard: Require java.sql to get access to java.sql

--- generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
+++ generator-parent/generator-standard/src/com.speedment.generator.standard/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.generator.standard {
+    requires java.sql;
     requires com.speedment.common.json;
     requires com.speedment.runtime.core;
     requires com.speedment.runtime.config;
