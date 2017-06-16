c2ded8ab68af0b6562e909bc0cd6e6955f32677d
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 12:17:26 +0200

    plugins.enums: Require com.speedment.runtime.typemapper to get access to com.speedment.runtime.typemapper

--- plugin-parent/enum-generator/src/com.speedment.plugins.enums/main/java/module-info.java
+++ plugin-parent/enum-generator/src/com.speedment.plugins.enums/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.plugins.enums {
+    requires com.speedment.runtime.typemapper;
     requires com.speedment.runtime.config;
     requires com.speedment.common.lazy;
     requires com.speedment.common.injector;
