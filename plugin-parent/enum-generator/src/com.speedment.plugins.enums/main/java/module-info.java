7dcc897218121efb3b77d9800f9f70aacb77357b
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 12:17:27 +0200

    plugins.enums: Require com.speedment.generator.translator to get access to com.speedment.generator.translator

--- plugin-parent/enum-generator/src/com.speedment.plugins.enums/main/java/module-info.java
+++ plugin-parent/enum-generator/src/com.speedment.plugins.enums/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.plugins.enums {
+    requires com.speedment.generator.translator;
     requires com.speedment.generator.standard;
     requires com.speedment.generator.core;
     requires com.speedment.common.codegen;
