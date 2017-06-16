b880f72ec3b43f93af0d1b7234fd54c2889bd519
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 12:15:12 +0200

    plugins.spring: Require spring.context to get access to org.springframework.context.annotation

--- plugin-parent/spring-generator/src/com.speedment.plugins.spring/main/java/module-info.java
+++ plugin-parent/spring-generator/src/com.speedment.plugins.spring/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.plugins.spring {
+    requires spring.context;
     requires com.speedment.runtime.config;
     requires com.speedment.generator.translator;
     requires com.speedment.common.codegen;
