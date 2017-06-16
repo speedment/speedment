dca4d227a3da987208d83c1bde8f8b684295b89c
Dan Lawesson <dan@kodgruvan.se>
2017-06-16 12:12:56 +0200

    plugins.json: Require com.speedment.runtime.config to get access to com.speedment.runtime.config

--- plugin-parent/json-stream/src/com.speedment.plugins.json/main/java/module-info.java
+++ plugin-parent/json-stream/src/com.speedment.plugins.json/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.speedment.plugins.json {
+    requires com.speedment.runtime.config;
     requires com.speedment.runtime.field;
     requires com.speedment.runtime.core;
     requires com.speedment.common.injector;
