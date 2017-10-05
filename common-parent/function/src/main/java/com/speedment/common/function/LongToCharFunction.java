4996955ab683ddf0066e588736ca3cd743332a43
pyknic <emil@speedment.com>
2017-10-05 13:26:34 -0700

    function: Create functional interfaces for LongTo___Function

---
+++ common-parent/function/src/main/java/com/speedment/common/function/LongToCharFunction.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * Copyright (c) 2006-2017, Speedment, Inc. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at: 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.speedment.common.function;
+
+/**
+ * Functional interface that corresponds to the method signature {@code char
+ * apply(long)}.
+ * 
+ * @author Emil Forslund
+ * @since  1.0.4
+ */
+public interface LongToCharFunction {
+    
+    /**
+     * Returns the {@code char} value for the specified {@code long}. This
+     * method should operate without side-effects.
+     * 
+     * @return the resulting value
+     */
+    char applyAsChar();
+}
