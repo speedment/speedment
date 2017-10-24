6f92869d266c98cbe7a8ae38f10121ddacf05db1
pyknic <emil@speedment.com>
2017-10-24 10:40:34 +0200

    tool-core: Create throttler utility class

---
+++ tool-parent/tool-core/src/main/java/com/speedment/tool/core/internal/util/Throttler.java
@@ -0,0 +1,45 @@
+package com.speedment.tool.core.internal.util;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Throttles requests so that they are only invoked at a maximum frequency. If
+ * more calls are made, others will be dropped.
+ * <p>
+ * This class is concurrent.
+ *
+ * @author Emil Forslund
+ * @since  3.0.16
+ */
+public final class Throttler {
+
+    private final ConcurrentHashMap<String, AtomicLong> timers;
+    private final long millis;
+
+    private Throttler(long millis) {
+        this.millis   = millis;
+        this.timers = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * Attempts to invoke the specified {@code runnable} if no other action with
+     * the same specifier has been called recently. Otherwise, the method will
+     * return with no effect.
+     *
+     * @param action    the action specifier
+     * @param runnable  the callable action to invoke
+     */
+    public void call(String action, Runnable runnable) {
+        final long now = now();
+        final AtomicLong timer = timers.computeIfAbsent(action, a -> new AtomicLong(0));
+        if (now == timer.updateAndGet(
+            lastCall -> lastCall + millis < now
+                ? now : lastCall
+        )) runnable.run();
+    }
+
+    private static long now() {
+        return System.currentTimeMillis();
+    }
+}
