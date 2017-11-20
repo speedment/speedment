eda0a916e5423e5348ee16c01082aa7870d9551f
Per-Åke Minborg <minborg@speedment.com>
2017-11-20 10:17:59 -0800

    runtime-bulk: Add changes not committed

--- runtime-parent/runtime-bulk/src/main/java/com/speedment/runtime/bulk/trait/package-info.java
+++ runtime-parent/runtime-bulk/src/main/java/com/speedment/runtime/bulk/trait/package-info.java
@@ -1,7 +1,8 @@
 /**
- * Bulk exposes interfaces to handle bulk modification operations on a data source such as a relational database.
+ * The bulk trait package exposes shared interfaces for bulk modification
+ * operations on a data source such as a relational database.
  * <p>
  * This package is part of the API. Modifications to classes here should only
  * (if ever) be done in major releases.
  */
-package com.speedment.runtime.bulk;
+package com.speedment.runtime.bulk.trait;
