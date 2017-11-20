7dc56e2cafbe67b36d817f73f5b323f1a1e0bf79
Per-Åke Minborg <minborg@speedment.com>
2017-11-20 10:04:38 -0800

    runtime-bulk: Add package-info, Fix #570

---
+++ runtime-parent/runtime-bulk/src/main/java/com/speedment/runtime/bulk/trait/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Bulk exposes interfaces to handle bulk modification operations on a data source such as a relational database.
+ * <p>
+ * This package is part of the API. Modifications to classes here should only
+ * (if ever) be done in major releases.
+ */
+package com.speedment.runtime.bulk;
