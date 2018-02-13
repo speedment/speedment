2c3609099e694244b8b9e09ef595857404859641
Emil Forslund <emil.forslund@pyknic.se>
2018-02-13 10:06:01 +0100

    generator-standard: Use Project Mutator instead of modifying map directly

--- generator-parent/generator-standard/src/main/java/com/speedment/generator/standard/lifecycle/GeneratedMetadataTranslator.java
+++ generator-parent/generator-standard/src/main/java/com/speedment/generator/standard/lifecycle/GeneratedMetadataTranslator.java
@@ -24,6 +24,7 @@ import com.speedment.common.injector.annotation.Inject;
 import com.speedment.common.json.Json;
 import com.speedment.generator.translator.AbstractJavaClassTranslator;
 import com.speedment.runtime.config.Project;
+import com.speedment.runtime.config.mutator.ProjectMutator;
 import com.speedment.runtime.config.util.DocumentTranscoder;
 import com.speedment.runtime.core.ApplicationMetadata;
 import com.speedment.runtime.core.component.InfoComponent;
@@ -74,9 +75,13 @@ public final class GeneratedMetadataTranslator extends AbstractJavaClassTranslat
 
         final Method initializer = Method.of("init", String.class).static_().private_();
 
-        final Project project = getSupport().projectOrThrow();
-        project.getData().put(Project.SPEEDMENT_VERSION, infoComponent.getEditionAndVersionString());
-        final List<String> lines = Stream.of(DocumentTranscoder.save(project, Json::toJson).split("\\R")).collect(toList());
+        final ProjectMutator<? extends Project> project = getSupport().projectOrThrow().mutator();
+        project.setSpeedmentVersion(infoComponent.getEditionAndVersionString());
+
+        final List<String> lines = Stream.of(
+            DocumentTranscoder.save(project.document(), Json::toJson)
+                .split("\\R")).collect(toList());
+
         final List<List<String>> segments = new ArrayList<>();
         List<String> segment = new ArrayList<>();
         segments.add(segment);
