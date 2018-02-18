9d62b07f4442ad6e3ec8482a3a23d84f02ca9a1e
Per-Åke Minborg <minborg@speedment.com>
2018-02-18 10:23:27 -0800

    runtime-join: Update package-info

--- runtime-parent/runtime-join/src/main/java/com/speedment/runtime/join/stage/package-info.java
+++ runtime-parent/runtime-join/src/main/java/com/speedment/runtime/join/stage/package-info.java
@@ -4,4 +4,4 @@
  * This package is part of the API. Modifications to classes here should only
  * (if ever) be done in major releases.
  */
-package com.speedment.runtime.join.pipeline;
+package com.speedment.runtime.join.stage;
