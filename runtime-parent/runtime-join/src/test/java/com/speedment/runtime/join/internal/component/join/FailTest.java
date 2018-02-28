8e53b838ceae29cfd6ee6726aa0bb5904760bf00
Per-Åke Minborg <minborg@speedment.com>
2018-02-27 18:10:03 -0800

    runtime-join: Test Jenkins Fail

---
+++ runtime-parent/runtime-join/src/test/java/com/speedment/runtime/join/internal/component/join/FailTest.java
@@ -0,0 +1,17 @@
+package com.speedment.runtime.join.internal.component.join;
+
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ *
+ * @author Per Minborg
+ */
+public final class FailTest {
+
+    @Test
+    public void innerJoin2() {
+        fail("Test Jenkins Fail");
+    }
+
+}
