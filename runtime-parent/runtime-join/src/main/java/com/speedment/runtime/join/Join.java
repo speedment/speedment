c304e328f101cf22a7b4a0214b91e68d63faa4fe
Per-Åke Minborg <minborg@speedment.com>
2018-03-05 18:33:09 -0800

    runtime-join: Improve stream documentation

--- runtime-parent/runtime-join/src/main/java/com/speedment/runtime/join/Join.java
+++ runtime-parent/runtime-join/src/main/java/com/speedment/runtime/join/Join.java
@@ -1,6 +1,8 @@
 package com.speedment.runtime.join;
 
+import com.speedment.runtime.join.trait.HasDefaultBuild;
 import com.speedment.runtime.join.trait.HasStream;
+import java.util.stream.Stream;
 
 /**
  * This interface represent a Join operation from which Streams of the joined
@@ -10,5 +12,20 @@ import com.speedment.runtime.join.trait.HasStream;
  * @param <T> the composite type of entity types
  */
 public interface Join<T> extends HasStream<T> {
+
+    /**
+     * {@inheritDoc }
+     * <p>
+     * Note: For Joins obtained via the default {@link HasDefaultBuild#build() }
+     * method, elements appearing in the stream may be <em>deeply immutable</em>
+     * meaning that Tuples in the stream are immutable and that entities
+     * contained in these Tuples may also be immutable. Thus, it is an error to
+     * invoke setters on any objects obtained directly or indirectly from the
+     * stream elements. If mutable objects are needed, the immutable objects
+     * must be used to create a new mutable object.
+     *
+     */
+    @Override
+    public Stream<T> stream();
 
 }
