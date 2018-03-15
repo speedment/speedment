5a7d491ab0f13f6fd4ef028ab20415164f25df32
Per-Åke Minborg <minborg@speedment.com>
2018-03-15 15:48:51 -0700

    runtime-join: Add swapEquivalent() method

--- runtime-parent/runtime-join/src/main/java/com/speedment/runtime/join/stage/JoinOperator.java
+++ runtime-parent/runtime-join/src/main/java/com/speedment/runtime/join/stage/JoinOperator.java
@@ -32,4 +32,33 @@ public enum JoinOperator {
         return symetric;
     }
 
+    /**
+     * Returns a JoinOperator that would produce an equivalent predicate when
+     * the arguments to the operator are applied swapped.
+     *
+     * @return a JoinOperator that would produce an equivalent predicate when
+     * the arguments to the operator are applied swapped.
+     * @throws IllegalStateException if no such equivalent JoinOperator exists
+     *
+     */
+    public JoinOperator swapEquivalent() {
+        switch (this) {
+            case EQUAL:
+                return EQUAL;
+            case NOT_EQUAL:
+                return NOT_EQUAL;
+            case LESS_THAN:
+                return GREATER_THAN;
+            case LESS_OR_EQUAL:
+                return GREATER_OR_EQUAL;
+            case GREATER_THAN:
+                return LESS_THAN;
+            case GREATER_OR_EQUAL:
+                return LESS_OR_EQUAL;
+        }
+        throw new IllegalStateException(
+            "There is no swap-equivalent " + JoinOperator.class.getSimpleName() + " to " + this
+        );
+    }
+
 }
