58cee605194d06e5b3c341432ca926c45087f42f
Per-Åke Minborg <minborg@speedment.com>
2018-04-27 19:49:28 -0700

    tuple: Add MutableTuple

---
+++ common-parent/tuple/src/main/java/com/speedment/common/tuple/MutableTuple.java
@@ -0,0 +1,77 @@
+/**
+ *
+ * Copyright (c) 2006-2017, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.speedment.common.tuple;
+
+import com.speedment.common.tuple.getter.TupleGetter;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * This interface defines a generic Tuple of any order that can hold null
+ * values. A Tuple is type safe, immutable and thread safe. For pure non-null
+ * value elements see {@link Tuple}
+ *
+ * @author pemi
+ * @see Tuple
+ *
+ */
+public interface MutableTuple extends BasicTuple<Optional<Object>> {
+
+    /**
+     * Returns a {@link Stream} of all values for this Tuple. If sequential, the
+     * Stream will start with the 0:th tuple and progress upwards.
+     *
+     * @return a {@link Stream} of all values for this Tuple
+     */
+    default Stream<Optional<Object>> stream() {
+        return IntStream.range(0, degree())
+            .mapToObj(this::get);
+    }
+
+    @Override
+    default <T> Stream<T> streamOf(Class<T> clazz) {
+        return stream()
+            .filter(clazz::isInstance)
+            .map(clazz::cast);
+    }
+
+     /**
+     * Returns a getter method for the specified ordinal element.
+     *
+     * @param index    the position of the element to return
+     * @param <TUPLE>  the type of the tuple
+     * @param <R>      the type of the returned element
+     * @return         the created getter
+     */
+    static <TUPLE extends MutableTuple, R> TupleGetter<TUPLE, R> getter(int index) {
+        return new TupleGetter<TUPLE, R>() {
+            @Override
+            public int index() {
+                return index;
+            }
+
+            @Override
+            @SuppressWarnings("unchecked")
+            public R apply(TUPLE tuple) {
+                return (R) tuple.get(index);
+            }
+        };
+    }
+    
+    
+}
---
+++ common-parent/tuple/src/test/java/com/speedment/common/tuple/mutable/MutableTuplesTest.java
@@ -0,0 +1,115 @@
+/**
+ *
+ * Copyright (c) 2006-2017, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.speedment.common.tuple.mutable;
+
+import com.speedment.common.tuple.MutableTuples;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+/**
+ * @author Per Minborg
+ */
+public class MutableTuplesTest {
+
+    @Test
+    public void degree0() {
+        final MutableTuple0 t0 = MutableTuples.create0();
+        test(t0);
+    }
+
+    @Test
+    public void constructor0() {
+        final MutableTuple0 t0 = MutableTuples.constructor().get();
+        test(t0);
+    }
+
+    @Test
+    public void degree1() {
+        final MutableTuple1<Integer> t0 = MutableTuples.create1();
+        test(t0);
+    }
+
+    @Test
+    public void constructor1() {
+        test(MutableTuples.constructor(Integer.class).get());
+    }
+
+    @Test
+    public void constructor5() {
+        final MutableTuple5<Integer, Integer, String, String, Long> t5 = MutableTuples.create5();
+        t5.set0(0);
+        t5.set1(1);
+        t5.set2("Olle");
+        t5.set3("Sven");
+        t5.set4(4L);
+
+        assertEquals((Integer) 0, t5.get0().get());
+        assertEquals((Integer) 1, t5.get1().get());
+        assertEquals("Olle", t5.get2().get());
+        assertEquals("Sven", t5.get3().get());
+        assertEquals((Long) 4L, t5.get4().get());
+
+        final List<Object> actual = t5.stream().map(Optional::get).collect(toList());
+        final List<Object> expected = Arrays.asList(0, 1, "Olle", "Sven", 4L);
+        assertEquals(expected, actual);
+
+        final List<Object> actualOf = t5.streamOf(String.class).collect(toList());
+        final List<Object> expectedOf = Arrays.asList("Olle", "Sven");
+        assertEquals(actualOf, expectedOf);
+
+    }
+
+    private void test(MutableTuple0 t0) {
+        assertEquals(0, t0.degree());
+
+        try {
+            t0.get(0);
+            fail();
+        } catch (IndexOutOfBoundsException ignore) {
+        }
+    }
+
+    private void test(MutableTuple1<Integer> t1) {
+        assertEquals(1, t1.degree());
+
+        final Optional<Integer> before = t1.get0();
+        assertFalse(before.isPresent());
+
+        t1.set0(1);
+        final Optional<Integer> after = t1.get0();
+        assertTrue(after.isPresent());
+        assertEquals((Integer) 1, after.get());
+
+        t1.set0(null);
+        final Optional<Integer> nulled = t1.get0();
+        assertFalse(nulled.isPresent());
+
+        try {
+            t1.get(1);
+            fail();
+        } catch (IndexOutOfBoundsException ignore) {
+        }
+    }
+
+}
