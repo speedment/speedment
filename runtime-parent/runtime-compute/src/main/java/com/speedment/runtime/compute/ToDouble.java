74c8c09e09f444871b2243a52aed01d7a14956e1
Per-Åke Minborg <minborg@speedment.com>
2018-05-03 18:35:51 -0700

    runtime-comput: Add missing (and redundant) method in ToDouble

--- runtime-parent/runtime-compute/src/main/java/com/speedment/runtime/compute/ToDouble.java
+++ runtime-parent/runtime-compute/src/main/java/com/speedment/runtime/compute/ToDouble.java
@@ -1,6 +1,7 @@
 package com.speedment.runtime.compute;
 
 import com.speedment.common.function.BooleanUnaryOperator;
+import com.speedment.common.function.FloatToDoubleFunction;
 import com.speedment.runtime.compute.expression.Expression;
 import com.speedment.runtime.compute.expression.ExpressionType;
 import com.speedment.runtime.compute.expression.Expressions;
@@ -79,6 +80,10 @@ extends Expression<T>,
     @Override
     default ToLong<T> asLong() {
         return CastUtil.castToLong(this);
+    }
+    
+    default ToDouble<T> mapToDouble(DoubleUnaryOperator operator) {
+        return MapperUtil.mapToDouble(this, operator);
     }
 
     @Override
