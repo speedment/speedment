edc2ad7fba4db7dec243fec88fcb1c98b23c52a2
Emil Forslund <emil.forslund@pyknic.se>
2018-05-08 13:19:49 +0200

    runtime-compute: Improve javadoc in specialized expressions

--- runtime-parent/runtime-compute/src/main/java/com/speedment/runtime/compute/expression/ComposedExpression.java
+++ runtime-parent/runtime-compute/src/main/java/com/speedment/runtime/compute/expression/ComposedExpression.java
@@ -3,16 +3,34 @@ package com.speedment.runtime.compute.expression;
 import java.util.function.Function;
 
 /**
- * @author Emil Forslund
+ * Specialized {@link Expression} interface used when a {@link #firstStep()}
+ * function is first applied to an incomming entity, before a
+ * {@link #secondStep()} expression is applied to get the result. This is
+ * typically used to implement operations like
+ * {@link Function#compose(Function)}.
+ *
  * @param <T> initial type
  * @param <A> intermediate type
- * 
+ *
+ * @author Emil Forslund
  * @since  3.1.0
  */
 public interface ComposedExpression<T, A> extends Expression<T> {
 
+    /**
+     * The function that is applied to incoming entities to get the value that
+     * is to be passed to {@link #secondStep()}.
+     *
+     * @return  the first step function
+     */
     Function<T, A> firstStep();
 
+    /**
+     * The inner expression that is applied to the result of
+     * {@link #firstStep()} to get the result of this full expression.
+     *
+     * @return  the second step expression
+     */
     Expression<A> secondStep();
 
 }
--- runtime-parent/runtime-compute/src/main/java/com/speedment/runtime/compute/expression/JoiningExpression.java
+++ runtime-parent/runtime-compute/src/main/java/com/speedment/runtime/compute/expression/JoiningExpression.java
@@ -15,12 +15,40 @@ import java.util.List;
  */
 public interface JoiningExpression<T> extends ToString<T> {
 
+    /**
+     * Returns an unmodifiable list of the expressions that are joined together
+     * in this expression. Whenever an entity is passed to this expression, the
+     * result will be the result of passing the same entity to all these
+     * expressions and joining them together with an optional {@link #prefix()},
+     * {@link #separator()} and {@link #suffix()}.
+     *
+     * @return  unmodifiable list of expressions that make up this expression
+     */
     List<ToString<T>> expressions();
 
+    /**
+     * Returns the prefix to put in front of the result of this expression. If
+     * no prefix is used, then this method will return an empty string.
+     *
+     * @return  the prefix
+     */
     CharSequence prefix();
 
+    /**
+     * Returns the suffix to put at the end of the result of this expression. If
+     * no suffix is used, then this method will return an empty string.
+     *
+     * @return  the suffix
+     */
     CharSequence suffix();
 
+    /**
+     * Returns the separator that will be placed in-between the results of all
+     * the {@link #expressions()} to separate them. If no separator is used,
+     * then this method will return an empty string.
+     *
+     * @return  the separator
+     */
     CharSequence separator();
 
 }
