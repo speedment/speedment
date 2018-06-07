93e6c1bab32b6fa8014c2b094e29a4a5d0c347e8
Dan Lawesson <dan@speedment.com>
2018-06-07 10:27:45 -0700

    connector-mysql: Fix #665 - driver version detection

--- runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/db/AbstractDbmsType.java
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/db/AbstractDbmsType.java
@@ -17,12 +17,9 @@
 package com.speedment.runtime.core.internal.db;
 
 import com.speedment.common.injector.Injector;
-import static com.speedment.common.injector.State.CREATED;
-import static com.speedment.common.injector.State.INITIALIZED;
 import com.speedment.common.injector.annotation.ExecuteBefore;
 import com.speedment.common.injector.annotation.Inject;
 import com.speedment.common.injector.annotation.WithState;
-import static com.speedment.common.invariant.LongRangeUtil.requireNonNegative;
 import com.speedment.runtime.config.Column;
 import com.speedment.runtime.core.component.DbmsHandlerComponent;
 import com.speedment.runtime.core.db.DatabaseNamingConvention;
@@ -33,10 +30,14 @@ import com.speedment.runtime.core.db.metadata.TypeInfoMetaData;
 import java.sql.Driver;
 import java.util.Collections;
 import java.util.List;
-import static java.util.Objects.requireNonNull;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
+
+import static com.speedment.common.injector.State.CREATED;
+import static com.speedment.common.injector.State.INITIALIZED;
+import static com.speedment.common.invariant.LongRangeUtil.requireNonNegative;
+import static java.util.Objects.requireNonNull;
 
 /**
  *
@@ -81,30 +82,35 @@ public abstract class AbstractDbmsType implements DbmsType {
     }
 
     protected boolean isSupported(String driverName) {
-        return driverClass(driverName).isPresent();
+        return driver(driverName).isPresent();
     }
 
-    protected Optional<Driver> driverClass(String driverName) {
+    protected Optional<Driver> driver(String driverName) {
         requireNonNull(driverName);
+        Driver driver = null;
         try {
-            return Optional.of(Class.forName(
+            final Class<?> driverClass = Class.forName(
                 driverName,
                 true,
                 injector.classLoader()
-            )).filter(Driver.class::isInstance)
-                .map(Driver.class::cast);
+            );
+            if (Driver.class.isAssignableFrom(driverClass)) {
+                driver = (Driver) driverClass.newInstance();
+            }
         } catch (final ClassNotFoundException ex) {
             try {
                 // Some JavaEE servers, notably Tomcat, runs the driver on the
                 // standard classloader.  This is the reason we need to check an
                 // extra time.
-                return Optional.of(Class.forName(driverName))
-                    .filter(Driver.class::isInstance)
-                    .map(Driver.class::cast);
-            } catch (final ClassNotFoundException ex2) {
+                final Class<?> driverClass = Class.forName(driverName);
+                if (Driver.class.isAssignableFrom(driverClass)) {
+                    driver = (Driver) driverClass.newInstance();
+                }
+            } catch (final ClassNotFoundException | IllegalAccessException | InstantiationException ex2) {
             }
+        } catch (IllegalAccessException | InstantiationException e) {
         }
-        return Optional.empty();
+        return Optional.ofNullable(driver);
     }
 
     @Override
