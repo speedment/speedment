0123db9287981e0d749f9f42dfecf1fb233ba9df
Dan Lawesson <dan@kodgruvan.se>
2018-08-27 16:39:48 +0200

    runtime-core: Simlpify FieldSetImpl

--- runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/manager/FieldSetImpl.java
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/manager/FieldSetImpl.java
@@ -1,10 +1,8 @@
 package com.speedment.runtime.core.internal.manager;
 
 import com.speedment.runtime.core.manager.FieldSet;
-import com.speedment.runtime.core.manager.Manager;
 import com.speedment.runtime.field.Field;
 
-import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,16 +10,8 @@ import java.util.stream.Stream;
 public class FieldSetImpl<ENTITY> implements FieldSet<ENTITY> {
     private final Set<String> includedIds;
 
-    private FieldSetImpl(Stream<Field<ENTITY>> fieldStream) {
+    public FieldSetImpl(Stream<Field<ENTITY>> fieldStream) {
         includedIds = fieldStream.map(f -> f.identifier().getColumnId()).collect(Collectors.toSet());
-    }
-
-    public FieldSetImpl(Manager<ENTITY> manager) {
-        this(manager.fields());
-    }
-
-    public FieldSetImpl(Field<ENTITY>[] fields) {
-        this(Arrays.stream(fields));
     }
 
     @Override
--- runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/manager/FieldSet.java
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/manager/FieldSet.java
@@ -3,15 +3,17 @@ package com.speedment.runtime.core.manager;
 import com.speedment.runtime.core.internal.manager.FieldSetImpl;
 import com.speedment.runtime.field.Field;
 
+import java.util.Arrays;
+
 public interface FieldSet<ENTITY> extends HasLabelSet<ENTITY> {
     FieldSet<ENTITY> except(Field<ENTITY> field);
 
     @SafeVarargs
     static <ENTITY> FieldSet<ENTITY> of(Field<ENTITY>... fields) {
-        return new FieldSetImpl<>(fields);
+        return new FieldSetImpl<>(Arrays.stream(fields));
     }
 
     static <ENTITY> FieldSet<ENTITY> fieldsOf(Manager<ENTITY> manager) {
-        return new FieldSetImpl<>(manager);
+        return new FieldSetImpl<>(manager.fields());
     }
 }
