a9565e004f5abd4444f5d10d459d1ef286406034
Dan Lawesson <dan@kodgruvan.se>
2018-12-04 16:48:05 +0100

    runtime-core: Map BIT(x) where x > 31 to Long, Fix #365

---
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/internal/db/RuleUtil.java
@@ -0,0 +1,25 @@
+package com.speedment.runtime.core.internal.db;
+
+import com.speedment.runtime.core.db.JavaTypeMap;
+
+import java.util.Optional;
+
+class RuleUtil {
+    /**
+     * All default rules that need more than just the jdbc type name to trigger
+     */
+    static final JavaTypeMap.Rule DEFAULT_OSS_RULE = (sqlTypeMapping, md) -> {
+        final String typeName   = md.getTypeName().toUpperCase();
+        final int columnSize    = md.getColumnSize();
+
+        switch (typeName) {
+            case "BIT": {
+                if (columnSize > 31) {  // jdbc will return unsigned 32 bit for BIT(32) which does not fit in Integer
+                    return Optional.of(Long.class);
+                }
+            }
+        }
+
+        return Optional.empty();
+    };
+}
