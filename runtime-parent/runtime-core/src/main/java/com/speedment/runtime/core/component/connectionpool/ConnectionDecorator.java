5eec1e933a1a814013082c516006a0a56ef16336
Per-Åke Minborg <minborg@speedment.com>
2019-09-05 14:59:41 +0200

    Bump version to 3.1.18 and fix warnings

--- runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/component/connectionpool/ConnectionDecorator.java
+++ runtime-parent/runtime-core/src/main/java/com/speedment/runtime/core/component/connectionpool/ConnectionDecorator.java
@@ -28,6 +28,7 @@ public interface ConnectionDecorator {
      * Configures a newly allocated connection before use.
      *
      * @param connection to apply configuration to
+     * @throws SQLException if unable to configure the connection
      * @throws NullPointerException if the provided {@code connection}
      * is {@code null}
      */
@@ -37,6 +38,7 @@ public interface ConnectionDecorator {
      * Cleans up a used connection before it is returned to a connection pool.
      *
      * @param connection to apply configuration to
+     * @throws SQLException if unable to cleanup the connection
      * @throws NullPointerException if the provided {@code connection}
      * is {@code null}
      */
