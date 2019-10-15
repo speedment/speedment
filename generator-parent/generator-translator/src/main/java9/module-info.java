676abf15782615aed40eac791ca1aa6dbd09836f
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-10-15 17:55:45 +0200

    translator: add common function module

--- generator-parent/generator-translator/src/main/java9/module-info.java
+++ generator-parent/generator-translator/src/main/java9/module-info.java
@@ -24,6 +24,7 @@ module com.speedment.generator.translator {
 
     requires com.speedment.common.annotation;
     requires com.speedment.common.codegen;
+    requires com.speedment.common.function;
     requires com.speedment.common.injector;
     requires com.speedment.common.mapstream;
 
