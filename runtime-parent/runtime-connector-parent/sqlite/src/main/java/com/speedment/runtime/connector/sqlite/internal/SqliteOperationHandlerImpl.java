d4d44a4c005d7fb41d39b68e9e4f2f8f4f17209c
Per-Åke Minborg <minborg@speedment.com>
2019-10-17 16:19:59 +0200

    sqlite: Use StandardDbmsOperationHandler, Work on #787

--- runtime-parent/runtime-connector-parent/sqlite/src/main/java/com/speedment/runtime/connector/sqlite/internal/SqliteOperationHandlerImpl.java
+++ runtime-parent/runtime-connector-parent/sqlite/src/main/java/com/speedment/runtime/connector/sqlite/internal/SqliteOperationHandlerImpl.java
@@ -16,11 +16,25 @@
  */
 package com.speedment.runtime.connector.sqlite.internal;
 
-import com.speedment.runtime.core.abstracts.AbstractDbmsOperationHandler;
+import com.speedment.common.injector.State;
+import com.speedment.common.injector.annotation.ExecuteBefore;
+import com.speedment.runtime.config.Dbms;
 import com.speedment.runtime.core.component.DbmsHandlerComponent;
 import com.speedment.runtime.core.component.connectionpool.ConnectionPoolComponent;
 import com.speedment.runtime.core.component.transaction.TransactionComponent;
+import com.speedment.runtime.core.db.AsynchronousQueryResult;
 import com.speedment.runtime.core.db.DbmsOperationHandler;
+import com.speedment.runtime.core.db.SqlFunction;
+import com.speedment.runtime.core.provider.StandardDbmsOperationHandler;
+import com.speedment.runtime.core.stream.parallel.ParallelStrategy;
+import com.speedment.runtime.field.Field;
+
+import java.sql.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.LongConsumer;
+import java.util.stream.Stream;
 
 /**
  * The implementation of {@link DbmsOperationHandler} for SQLite databases.
@@ -28,14 +42,99 @@ import com.speedment.runtime.core.db.DbmsOperationHandler;
  * @author Emil Forslund
  * @since  3.1.10
  */
-public final class SqliteOperationHandlerImpl extends AbstractDbmsOperationHandler {
+public final class SqliteOperationHandlerImpl implements DbmsOperationHandler {
+
+    private final StandardDbmsOperationHandler inner;
 
     public SqliteOperationHandlerImpl(
         final ConnectionPoolComponent connectionPoolComponent,
         final DbmsHandlerComponent dbmsHandlerComponent,
         final TransactionComponent transactionComponent
     ) {
-        super(connectionPoolComponent, dbmsHandlerComponent, transactionComponent);
+        inner = new StandardDbmsOperationHandler(connectionPoolComponent, dbmsHandlerComponent, transactionComponent);
     }
 
+    @ExecuteBefore(State.STOPPED)
+    public void close() {
+        inner.close();
+    }
+
+    @Override
+    public <T> Stream<T> executeQuery(Dbms dbms, String sql, List<?> values, SqlFunction<ResultSet, T> rsMapper) {
+        return inner.executeQuery(dbms, sql, values, rsMapper);
+    }
+
+    @Override
+    public <T> AsynchronousQueryResult<T> executeQueryAsync(Dbms dbms, String sql, List<?> values, SqlFunction<ResultSet, T> rsMapper, ParallelStrategy parallelStrategy) {
+        return inner.executeQueryAsync(dbms, sql, values, rsMapper, parallelStrategy);
+    }
+
+    @Override
+    public <ENTITY> void executeInsert(Dbms dbms, String sql, List<?> values, Collection<Field<ENTITY>> generatedKeyFields, Consumer<List<Long>> generatedKeyConsumer) throws SQLException {
+        inner.executeInsert(dbms, sql, values, generatedKeyFields, generatedKeyConsumer);
+    }
+
+    @Override
+    public void executeUpdate(Dbms dbms, String sql, List<?> values) throws SQLException {
+        inner.executeUpdate(dbms, sql, values);
+    }
+
+    @Override
+    public void executeDelete(Dbms dbms, String sql, List<?> values) throws SQLException {
+        inner.executeDelete(dbms, sql, values);
+    }
+
+    @Override
+    public void handleGeneratedKeys(PreparedStatement ps, LongConsumer longConsumer) throws SQLException {
+        inner.handleGeneratedKeys(ps, longConsumer);
+    }
+
+    @Override
+    public Clob createClob(Dbms dbms) throws SQLException {
+        return inner.createClob(dbms);
+    }
+
+    @Override
+    public Blob createBlob(Dbms dbms) throws SQLException {
+        return inner.createBlob(dbms);
+    }
+
+    @Override
+    public NClob createNClob(Dbms dbms) throws SQLException {
+        return inner.createNClob(dbms);
+    }
+
+    @Override
+    public SQLXML createSQLXML(Dbms dbms) throws SQLException {
+        return inner.createSQLXML(dbms);
+    }
+
+    @Override
+    public Array createArray(Dbms dbms, String typeName, Object[] elements) throws SQLException {
+        return inner.createArray(dbms, typeName, elements);
+    }
+
+    @Override
+    public Struct createStruct(Dbms dbms, String typeName, Object[] attributes) throws SQLException {
+        return inner.createStruct(dbms, typeName, attributes);
+    }
+
+    public String encloseField(Dbms dbms, String fieldName) {
+        return inner.encloseField(dbms, fieldName);
+    }
+
+    @Override
+    public <T> Stream<T> executeQuery(Dbms dbms, String sql, SqlFunction<ResultSet, T> rsMapper) {
+        return inner.executeQuery(dbms, sql, rsMapper);
+    }
+
+    @Override
+    public void configureSelect(PreparedStatement statement) throws SQLException {
+        inner.configureSelect(statement);
+    }
+
+    @Override
+    public void configureSelect(ResultSet resultSet) throws SQLException {
+        inner.configureSelect(resultSet);
+    }
 }
