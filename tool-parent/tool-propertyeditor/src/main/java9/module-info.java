cdc3a45a2a7a94c9cfb8b4d6a7854bc49c4409a6
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-10-17 15:41:54 +0200

    tool-propertyeditor: add requires transitive statements in module-info

--- tool-parent/tool-propertyeditor/src/main/java9/module-info.java
+++ tool-parent/tool-propertyeditor/src/main/java9/module-info.java
@@ -21,14 +21,13 @@ module com.speedment.tool.propertyeditor {
     exports com.speedment.tool.propertyeditor.item;
     exports com.speedment.tool.propertyeditor.provider;
 
-    requires com.speedment.common.injector;
-    requires com.speedment.common.mapstream;
-
-    requires com.speedment.runtime.core;
-    requires com.speedment.runtime.config;
     requires com.speedment.runtime.typemapper;
 
     requires com.speedment.generator.translator;
 
-    requires com.speedment.tool.config;
+    requires transitive com.speedment.common.injector;
+    requires transitive com.speedment.common.mapstream;
+    requires transitive com.speedment.runtime.core;
+    requires transitive com.speedment.runtime.config;
+    requires transitive com.speedment.tool.config;
 }
