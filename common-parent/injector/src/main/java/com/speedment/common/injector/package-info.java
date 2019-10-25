564c656d6ec4ec0045fcd2433e70974ded449a64
Carina Dreifeldt <carina@speedment.com>
2019-10-25 11:14:50 +0200

    Injector: Add package-info

---
+++ common-parent/injector/src/main/java/com/speedment/common/injector/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The base package of Injector. Only the most commonly used classes reside
+ * directly in this package.
+ * <p>
+ * This package is part of the API. Modifications to classes here should only
+ * (if ever) be done in major releases.
+ */
+package com.speedment.common.injector;
