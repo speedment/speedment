d0ff181d6c400fa3f73d482991685e5e00e84893
Carina Dreifeldt <carina@speedment.com>
2019-10-25 11:32:26 +0200

    Codegen: Add package-info

---
+++ common-parent/codegen/src/main/java/com/speedment/common/codegen/model/value/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains value classes.
+ * <p>
+ * This package is part of the API. Modifications to classes here should only
+ * (if ever) be done in major releases.
+ */
+package com.speedment.common.codegen.model.value;
---
+++ common-parent/codegen/src/main/java/com/speedment/common/codegen/provider/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains provider classes.
+ * <p>
+ * This package is part of the API. Modifications to classes here should only
+ * (if ever) be done in major releases.
+ */
+package com.speedment.common.codegen.provider;
