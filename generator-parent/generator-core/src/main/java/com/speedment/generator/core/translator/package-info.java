53c92a1696761383cd899e89839a21e99e020b17
Carina Dreifeldt <carina@speedment.com>
2019-10-25 12:30:02 +0200

    generator-core: Add package info

--- generator-parent/generator-core/src/main/java/com/speedment/generator/core/translator/package-info.java
+++ generator-parent/generator-core/src/main/java/com/speedment/generator/core/translator/package-info.java
@@ -1,7 +1,7 @@
 /**
- * Fundamental Code Generation components is located in this package.
+ * This package contains translator classes.
  * <p>
  * This package is part of the API. Modifications to classes here should only
  * (if ever) be done in major releases.
  */
-package com.speedment.generator.core;
+package com.speedment.generator.core.translator;
