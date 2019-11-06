f95f24c0206fbc322e2341e5d0f63bda87707759
Per-Åke Minborg <minborg@speedment.com>
2019-11-06 17:48:57 +0100

    Remove module mutable-streams, Fix #816

--- common-parent/mutablestream/src/main/java/com/speedment/common/mutablestream/source/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains source classes.
- * <p>
- * This package is part of the API. Modifications to classes here should only
- * (if ever) be done in major releases.
- */
-package com.speedment.common.mutablestream.source;
--- common-parent/mutablestream/src/main/java/com/speedment/common/mutablestream/terminate/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains terminate classes.
- * <p>
- * This package is part of the API. Modifications to classes here should only
- * (if ever) be done in major releases.
- */
-package com.speedment.common.mutablestream.terminate;
