fc997747b24a71a1d3d6375e3f8e50034e28e4c8
Per-Åke Minborg <minborg@speedment.com>
2019-11-15 15:59:58 +0100

    json: Fix SQ minors

--- common-parent/json/src/main/java/com/speedment/common/json/internal/JsonDeserializer.java
+++ common-parent/json/src/main/java/com/speedment/common/json/internal/JsonDeserializer.java
@@ -21,7 +21,6 @@ import com.speedment.common.json.JsonSyntaxException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
