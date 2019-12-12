931b2561ebfafe10ee5938ac477765a48d297132
Mislav Milicevic <milicevic.mislav@nsoft.com>
2019-12-12 19:27:10 +0100

    compute: more tests

---
+++ runtime-parent/runtime-compute/src/test/java/com/speedment/runtime/compute/expression/predicate/NullPredicateTest.java
@@ -0,0 +1,57 @@
+/*
+ *
+ * Copyright (c) 2006-2019, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.speedment.runtime.compute.expression.predicate;
+
+import com.speedment.runtime.compute.trait.ToNullable;
+import org.junit.jupiter.api.Test;
+
+final class NullPredicateTest {
+
+    @Test
+    void nullPredicateType() {
+        new DummyNullPredicate(NullPredicateType.IS_NULL);
+        new DummyNullPredicate(NullPredicateType.IS_NOT_NULL);
+        new DummyNullPredicate(NullPredicateType.OTHER);
+    }
+
+    private static final class DummyNullPredicate implements NullPredicate<String, String> {
+
+        private final NullPredicateType nullPredicateType;
+
+        private DummyNullPredicate(
+                NullPredicateType nullPredicateType) {
+            this.nullPredicateType = nullPredicateType;
+        }
+
+        @Override
+        public NullPredicateType nullPredicateType() {
+            return nullPredicateType;
+        }
+
+        @Override
+        public ToNullable<String, String, ?> expression() {
+            return null;
+        }
+
+        @Override
+        public boolean test(String s) {
+            return false;
+        }
+    }
+
+}
