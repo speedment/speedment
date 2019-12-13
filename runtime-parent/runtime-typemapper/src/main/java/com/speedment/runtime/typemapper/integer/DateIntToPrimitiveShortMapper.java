1f9b8b0c2b207b4ec7bd26c78bd07fab6442d006
Julia Gustafsson <gustafsson.julia@live.se>
2019-12-13 17:59:10 +0100

    typemapper: Fix conversion from DateInt to Short, Fix #845

--- runtime-parent/runtime-typemapper/src/main/java/com/speedment/runtime/typemapper/integer/DateIntToPrimitiveShortMapper.java
+++ runtime-parent/runtime-typemapper/src/main/java/com/speedment/runtime/typemapper/integer/DateIntToPrimitiveShortMapper.java
@@ -20,6 +20,8 @@ import com.speedment.runtime.config.Column;
 import com.speedment.runtime.typemapper.TypeMapper;
 
 import java.lang.reflect.Type;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 /**
  * Specialized Type Mapper that stores dates on the form {@code YYYYMMDD} as
@@ -32,10 +34,6 @@ import java.lang.reflect.Type;
 public final class DateIntToPrimitiveShortMapper
 implements TypeMapper<Integer, Short> {
 
-    private static final int DAY_MASK   = 0b0000_0000_0001_1111;
-    private static final int MONTH_MASK = 0b0000_0001_1110_0000;
-    private static final int YEAR_MASK  = 0b1111_1110_0000_0000;
-    
     @Override
     public String getLabel() {
         return "Date Integer to short (primitive)";
@@ -47,26 +45,22 @@ implements TypeMapper<Integer, Short> {
     }
 
     @Override
-    public Short toJavaType(Column column, Class<?> entityType, Integer value) {
-        if (value == null) return null;
-        final int date = value;
+    public Short toJavaType(Column column, Class<?> entityType, Integer date) {
+        if (date == null) return null;
+        int day = date % 100;
+        int month = (date % 10_000 - day) / 100;
+        int year = date / 10_000;
 
-        return (short) (
-            ((date - (date / 100) * 100) & DAY_MASK) |
-            ((((date / 100) - ((date / 10_000) * 100)) << 5) & MONTH_MASK) |
-            (((date / 10_000 - 1970) << 9) & YEAR_MASK)
-        );
+        LocalDate localDate = LocalDate.of(year, month, day);
+        short daysSinceEpoch = (short) ChronoUnit.DAYS.between(localDate, LocalDate.of(1970, 1,1));
+        return daysSinceEpoch;
     }
 
     @Override
     public Integer toDatabaseType(Short value) {
         if (value == null) return null;
-        final short encoded = value;
-        
-        final int day   =   encoded & DAY_MASK;
-        final int month =  (encoded & MONTH_MASK) >>> 5;
-        final int year  = ((encoded & YEAR_MASK ) >>> 9) + 1970;
-        
-        return 10_000 * year + 100 * month + day;
+
+        LocalDate date = LocalDate.ofEpochDay(value);
+        return date.getYear() * 10_000 + date.getMonthValue() * 100 + date.getDayOfMonth();
     }
 }
