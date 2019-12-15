30570fa84baa42c11c6307ca5065931f17e9dfce
Per-Åke Minborg <minborg@speedment.com>
2019-12-15 18:15:15 +0100

    mapstream: update tests

--- common-parent/mapstream/src/test/java/com/speedment/common/mapstream/MapStreamTest.java
+++ common-parent/mapstream/src/test/java/com/speedment/common/mapstream/MapStreamTest.java
@@ -1,19 +1,19 @@
 package com.speedment.common.mapstream;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.util.AbstractMap;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.stream.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class MapStreamTest {
+
+    private final double EPSILON = 1e-9;
 
     private  Map<String, Integer> stringToint;
     private  Map<Integer, String> intToString;
@@ -89,134 +89,217 @@ final class MapStreamTest {
     @Test
     void flip() {
         final MapStream<Integer, String> ms = MapStream.flip(instance);
-        assertEquals(MapStream.of(intToString), ms);
+        assertEquals(MapStream.of(intToString).toMap(), ms.toMap());
     }
 
     @Test
     void filter() {
         final Map<String, Integer> expected = refStream().filter(KEY_STARTS_WITH_D).collect(TO_MAP);
-
-
-        final MapStream<String, Integer> ms = MapStream.of(stringToint);
-
         final Map<String, Integer> actual = instance.filter(KEY_STARTS_WITH_D).toMap();
         assertEquals(expected, actual);
     }
 
     @Test
     void testFilter() {
+        final Map<String, Integer> expected = refStream().filter(KEY_STARTS_WITH_D).collect(TO_MAP);
+        final Map<String, Integer> actual = instance.filter((k, v) -> k.startsWith("d")).toMap();
+        assertEquals(expected, actual);
     }
 
     @Test
     void filterKey() {
+        final Map<String, Integer> expected = refStream().filter(KEY_STARTS_WITH_D).collect(TO_MAP);
+        final Map<String, Integer> actual = instance.filterKey(k -> k.startsWith("d")).toMap();
+        assertEquals(expected, actual);
     }
 
     @Test
     void filterValue() {
+        final Map<String, Integer> expected = refStream().filter(KEY_STARTS_WITH_D).collect(TO_MAP);
+        final Map<String, Integer> actual = instance.filterValue(k -> k.equals(2)).toMap();
+        assertEquals(expected, actual);
     }
 
     @Test
     void map() {
+        final Set<String> expected = new HashSet<>(stringToint.keySet());
+        final Set<String> actual = instance.map(Map.Entry::getKey).collect(Collectors.toSet());
+        assertEquals(expected, actual);
     }
 
     @Test
     void testMap() {
+        final Set<String> expected = new HashSet<>(stringToint.keySet());
+        final Set<String> actual = instance.map((k, v) -> k).collect(Collectors.toSet());
+        assertEquals(expected, actual);
     }
 
     @Test
     void mapKey() {
+        final Map<String, Integer> expected = refStream().map(e -> new AbstractMap.SimpleImmutableEntry<>(e.getKey() + "A", e.getValue())).collect(TO_MAP);
+        final Map<String, Integer> actual = instance.mapKey(s -> s + "A").toMap();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testMapKey() {
+        final Map<String, Integer> expected = refStream().map(e -> new AbstractMap.SimpleImmutableEntry<>(e.getKey() + "A", e.getValue())).collect(TO_MAP);
+        final Map<String, Integer> actual = instance.mapKey((k, v) -> k + "A").toMap();
+        assertEquals(expected, actual);
     }
 
     @Test
     void mapValue() {
+        final Map<String, Integer> expected = refStream().map(e -> new AbstractMap.SimpleImmutableEntry<>(e.getKey(), e.getValue() + 1)).collect(TO_MAP);
+        final Map<String, Integer> actual = instance.mapValue(v -> v + 1).toMap();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testMapValue() {
+        final Map<String, Integer> expected = refStream().map(e -> new AbstractMap.SimpleImmutableEntry<>(e.getKey(), e.getValue() + 1)).collect(TO_MAP);
+        final Map<String, Integer> actual = instance.mapValue((k, v) -> v + 1).toMap();
+        assertEquals(expected, actual);
     }
 
     @Test
     void mapToInt() {
+        final int expected = refStream().mapToInt(Map.Entry::getValue).map(i -> i + 1).sum();
+        final int actual = instance.mapToInt(e -> e.getValue() + 1).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testMapToInt() {
+        final int expected = refStream().mapToInt(Map.Entry::getValue).map(i -> i + 1).sum();
+        final int actual = instance.mapToInt((k, v) -> v + 1).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void mapToLong() {
+        final long expected = refStream().mapToLong(Map.Entry::getValue).map(i -> i + 1).sum();
+        final long actual = instance.mapToLong(e -> e.getValue() + 1).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testMapToLong() {
+        final long expected = refStream().mapToLong(Map.Entry::getValue).map(i -> i + 1).sum();
+        final long actual = instance.mapToLong((k, v) -> v + 1).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void mapToDouble() {
+        final double expected = refStream().mapToDouble(Map.Entry::getValue).map(i -> i + 1).sum();
+        final double actual = instance.mapToDouble(e -> e.getValue() + 1).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testMapToDouble() {
+        final double expected = refStream().mapToDouble(Map.Entry::getValue).map(i -> i + 1).sum();
+        final double actual = instance.mapToDouble((k, v) -> v + 1).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void flatMap() {
+        final long expected = refStream().flatMap(e -> e.getKey().chars().boxed()).count();
+        final long actual = instance.flatMap(e -> e.getKey().chars().boxed()).count();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testFlatMap() {
+        final long expected = refStream().flatMap(e -> e.getKey().chars().boxed()).count();
+        final long actual = instance.flatMap((k, v) -> k.chars().boxed()).count();
+        assertEquals(expected, actual);
     }
 
     @Test
     void flatMapKey() {
+        final long expected = refStream().flatMap(e -> e.getKey().chars().boxed()).count();
+        final long actual = instance.flatMapKey(e -> e.chars().boxed()).count();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testFlatMapKey() {
+        final long expected = refStream().flatMap(e -> e.getKey().chars().boxed()).count();
+        final long actual = instance.flatMapKey((k, v) -> k.chars().boxed()).count();
+        assertEquals(expected, actual);
     }
 
     @Test
     void flatMapValue() {
+        final long expected = refStream().flatMap(e -> IntStream.range(0, e.getValue()).boxed()).count();
+        final long actual = instance.flatMapValue(v -> IntStream.range(0, v).boxed()).count();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testFlatMapValue() {
+        final long expected = refStream().flatMap(e -> IntStream.range(0, e.getValue()).boxed()).count();
+        final long actual = instance.flatMapValue((k, v) -> IntStream.range(0, v).boxed()).count();
+        assertEquals(expected, actual);
     }
 
     @Test
     void flatMapToInt() {
+        final int expected = refStream().flatMapToInt(e -> IntStream.range(0, e.getValue())).sum();
+        final int actual = instance.flatMapToInt(e -> IntStream.range(0, e.getValue())).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testFlatMapToInt() {
+        final int expected = refStream().flatMapToInt(e -> IntStream.range(0, e.getValue())).sum();
+        final int actual = instance.flatMapToInt((k, v) -> IntStream.range(0, v)).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void flatMapToLong() {
+        final long expected = refStream().flatMapToLong(e -> LongStream.range(0, e.getValue())).sum();
+        final long actual = instance.flatMapToLong(e -> LongStream.range(0, e.getValue())).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void testFlatMapToLong() {
+        final long expected = refStream().flatMapToLong(e -> LongStream.range(0, e.getValue())).sum();
+        final long actual = instance.flatMapToLong((k, v) -> LongStream.range(0, v)).sum();
+        assertEquals(expected, actual);
     }
 
     @Test
     void flatMapToDouble() {
+        final double expected = refStream().flatMapToDouble(e -> LongStream.range(0, e.getValue()).mapToDouble(l -> l)).sum();
+        final double actual = instance.flatMapToDouble(e -> LongStream.range(0, e.getValue()).mapToDouble(l -> l)).sum();
+        assertEquals(expected, actual, EPSILON);
     }
 
     @Test
     void testFlatMapToDouble() {
+        final double expected = refStream().flatMapToDouble(e -> LongStream.range(0, e.getValue()).mapToDouble(l -> l)).sum();
+        final double actual = instance.flatMapToDouble((k, v) -> LongStream.range(0, v).mapToDouble(l -> l)).sum();
+        assertEquals(expected, actual, EPSILON);
     }
 
     @Test
     void keys() {
+        final List<String> expected = refStream().map(Map.Entry::getKey).collect(Collectors.toList());
+        final List<String> actual = instance.keys().collect(Collectors.toList());
+        assertEquals(expected, actual);
     }
 
     @Test
     void values() {
+        final List<Integer> expected = refStream().map(Map.Entry::getValue).collect(Collectors.toList());
+        final List<Integer> actual = instance.values().collect(Collectors.toList());
+        assertEquals(expected, actual);
     }
 
     @Test
